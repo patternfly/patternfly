9a477f80cc42a0c163d40c3f4fdaef689aecbd93
Cliff Pyles <cpyles@redhat.com>
2018-02-22 04:05:35 -0500

    Add support for Markdown pages and documentation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,79 @@
 const path = require('path')
 const inflection = require('inflection')
 const StyleLintPlugin = require('stylelint-webpack-plugin')
+
+exports.onCreateNode = ({ node, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators
+  const PAGES_BASE_DIR = path.resolve(__dirname, './src/pages')
+  const PATTERNS_BASE_DIR = path.resolve(__dirname, './src/patternfly/patterns')
+  const COMPONENTS_BASE_DIR = path.resolve(__dirname, './src/patternfly/components')
+  const isMarkdown = (node.internal.type === 'MarkdownRemark')
+
+  if (isMarkdown) {
+    const isPage = (node.fileAbsolutePath.includes(PAGES_BASE_DIR))
+    const isPattern = (node.fileAbsolutePath.includes(PATTERNS_BASE_DIR))
+    const isComponent = (node.fileAbsolutePath.includes(COMPONENTS_BASE_DIR))
+
+    if (isPage) {
+      let relativePath = path.relative(PAGES_BASE_DIR, node.fileAbsolutePath)
+      let pagePath = `/${relativePath}`.replace(/\.md$/, '')
+      createNodeField({ node, name: 'path', value: pagePath })
+      createNodeField({ node, name: 'type', value: 'page' })
+      createNodeField({ node, name: 'contentType', value: 'page' })
+    } else if (isComponent) {
+      let componentName = path.basename(path.dirname(node.fileAbsolutePath))
+      let componentSlug = inflection.dasherize(componentName)
+      let pagePath = `/components/${componentName}/docs`
+      createNodeField({ node, name: 'path', value: pagePath })
+      createNodeField({ node, name: 'type', value: 'documentation' })
+      createNodeField({ node, name: 'contentType', value: 'component' })
+    } else if (isPattern) {
+      let patternName = path.basename(path.dirname(node.fileAbsolutePath))
+      let patternSlug = inflection.dasherize(patternName)
+      let pagePath = `/patterns/${patternName}/docs`
+      createNodeField({ node, name: 'path', value: pagePath })
+      createNodeField({ node, name: 'type', value: 'documentation' })
+      createNodeField({ node, name: 'contentType', value: 'pattern' })
+    }
+  }
+}
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              type
+              path
+              contentType
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    return result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.path,
+        component: path.resolve(__dirname, `./src/templates/${node.fields.type}.js`),
+        context: {
+          path: node.fields.path,
+          type: node.fields.type,
+          contentType: node.fields.contentType
+        }
+      })
+    })
+  })
+}
+
 exports.onCreatePage = async ({ page, boundActionCreators }) => {
   const { createPage, createNodeField } = boundActionCreators
   const CATEGORY_REGEX = /^\/(components|patterns)\/$/
@@ -13,7 +86,6 @@ exports.onCreatePage = async ({ page, boundActionCreators }) => {
     page.context.title = ''
 
     if (page.path.match(CATEGORY_REGEX)) {
-      // page.layout = 'category'
       page.context.type = 'category'
       page.context.category = page.path.match(CATEGORY_REGEX)[1]
     } else if (page.path.match(CATEGORY_CHILD_REGEX)) {
@@ -21,7 +93,6 @@ exports.onCreatePage = async ({ page, boundActionCreators }) => {
       let pageSlug = page.path.match(CATEGORY_CHILD_REGEX)[2]
       let pageName = pageSlug.replace('-', ' ')
       let pageTitle = inflection.titleize(pageName)
-      // page.layout = 'single'
       page.context.type = inflection.singularize(pageCategory)
       page.context.category = pageCategory
       page.context.slug = pageSlug
@@ -42,6 +113,7 @@ exports.modifyWebpackConfig = ({ config, stage }) => {
         '@siteComponents': path.resolve(__dirname, './src/_site'),
         '@patterns': path.resolve(__dirname, './src/patternfly/patterns'),
         '@components': path.resolve(__dirname, './src/patternfly/components'),
+        '@project': path.resolve(__dirname, './src')
       }
     },
     plugins: [
---
+++ src/templates/documentation.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default ({data}) => {
+  let { fields, html } = data.markdownRemark
+  return (
+    <div className="page-template">
+      <div className="page-template__content" dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  )
+}
+
+export const GetDocumentationContent = graphql`
+  query GetDocumentationContentByPath($path: String!) {
+    markdownRemark(fields: { path: { eq: $path } }) {
+      html
+      fields {
+        path
+        type
+        contentType
+      }
+    }
+  }
+`
