dfa3226ef902a00bd4d9d73d8647e31689622d16
Cliff Pyles <cpyles@redhat.com>
2018-02-22 04:57:08 -0500

    Add Secondary Nav back to the workspace

---
+++ src/pages/patterns/secondary-nav/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Example from '@siteComponents/Example'
+import SecondaryNav from '@patterns/SecondaryNav'
+
+export default () => {
+  return (
+    <div>
+      <Example heading="Example">
+        <SecondaryNav>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </SecondaryNav>
+      </Example>
+    </div>
+  )
+}
---
+++ src/patternfly/patterns/SecondaryNav/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './styles.scss'
+
+export default ({children, className = ''}) => {
+  return (
+    <ul className="pf-p-secondary-nav" role="tablist">
+      <li className="pf-p-secondary-nav__item">
+        <a href="#" role="tab" className="pf-p-secondary-nav__link pf-is-active" aria-selected="true">Secondary Tab One</a>
+      </li>
+      <li className="pf-p-secondary-nav__item">
+        <a href="#" role="tab" className="pf-p-secondary-nav__link">Secondary Tab Two</a>
+      </li>
+      <li className="pf-p-secondary-nav__item">
+        <a href="#" role="tab" className="pf-p-secondary-nav__link">Secondary Tab Three</a>
+      </li>
+      <li className="pf-p-secondary-nav__item">
+        <a href="#" role="tab" className="pf-p-secondary-nav__link pf-is-disabled" aria-disabled="true">Secondary Tab Disabled</a>
+      </li>
+      <li className="pf-p-secondary-nav__item">
+        <a href="#" role="tab" className="pf-p-secondary-nav__link">Secondary Tab Five</a>
+      </li>
+    </ul>
+
+  )
+}
