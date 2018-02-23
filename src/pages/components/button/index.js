51cc4804dac4560eb819836375f0d1db4fa6445d
Cliff Pyles <cpyles@redhat.com>
2018-02-23 16:15:43 -0500

    Add Button examples

--- src/pages/components/button/index.js
+++ src/pages/components/button/index.js
@@ -6,10 +6,20 @@ import Button, { Docs } from '@components/Button'
 export default () => {
   return (
     <Documentation docs={Docs}>
-      <Example heading="Example">
-        <Button>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </Button>
+      <Example heading="Button Types">
+        <Button>Default</Button>
+        <Button className="pf-is-primary">Primary</Button>
+        <Button className="pf-is-secondary">Secondary</Button>
+        <Button className="pf-is-tertiary">Tertiary</Button>
+        <Button className="pf-is-destructive">Destructive</Button>
+      </Example>
+      <Example heading="Button Sizes">
+        <Button className="pf-is-small">Small</Button>
+        <Button className="pf-is-large">Large</Button>
+      </Example>
+      <Example heading="Button States">
+        <Button className="pf-is-focus">Focused</Button>
+        <Button className="pf-is-disabled">Disabled</Button>
       </Example>
     </Documentation>
   )
