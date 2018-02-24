38420784d589e2390dc048dc085049acd38b7b3a
Cliff Pyles <cpyles@redhat.com>
2018-02-24 08:09:52 -0500

    Make Overlay dismissable in the workspace

--- src/pages/components/overlay/index.js
+++ src/pages/components/overlay/index.js
@@ -3,14 +3,33 @@ import Documentation from '@siteComponents/Documentation'
 import Example from '@siteComponents/Example'
 import Overlay, { Docs } from '@components/Overlay'
 
-export default () => {
-  return (
-    <Documentation docs={Docs}>
-      <Example heading="Example">
-        <Overlay>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </Overlay>
-      </Example>
-    </Documentation>
-  )
+class OverlayPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      overlayClassName: 'pf-is-active'
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      overlayClassName: ''
+    }))
+  }
+
+  render() {
+    return (
+      <Documentation docs={Docs}>
+        <Example heading="Example">
+          <Overlay
+            className={this.state.overlayClassName}
+            onClick={this.handleClick}>
+          </Overlay>
+        </Example>
+      </Documentation>
+    )
+  }
 }
+
+export default OverlayPage
