99b746a162d28e9e36c4560226a2b83ef86c29ea
Cliff Pyles <cpyles@redhat.com>
2018-03-05 08:03:03 -0500

    Add pf-p-notification-drawer

--- src/patternfly/components/Dialog/index.js
+++ src/patternfly/components/Dialog/index.js
@@ -4,10 +4,26 @@ import './styles.scss'
 
 export const Docs = docs
 
-export default ({children, className = ''}) => {
+export default ({children, className = '', heading = ''}) => {
+  let DialogHeader = ''
+  
+  if (heading.length) {
+    DialogHeader = (
+      <div className="pf-c-dialog__header">
+        <h3 className="pf-c-dialog_heading">{heading}</h3>
+        <button className="pf-c-dialog_close-button">
+          <i className="fas fa-times"></i>
+        </button>
+      </div>
+    )
+  }
+
   return (
     <div className={`pf-c-dialog ${className}`}>
-      {children}
+      {DialogHeader}
+      <div className="pf-c-dialog__main">
+        {children}
+      </div>
     </div>
   )
 }
