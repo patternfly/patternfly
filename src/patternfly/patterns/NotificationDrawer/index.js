caec43a5992ea0e4da9f86099f864a94f6cf7bd7
Cliff Pyles <cpyles@redhat.com>
2018-03-12 10:43:16 -0400

    Add notification examples into the notification drawer pattern

--- src/patternfly/patterns/NotificationDrawer/index.js
+++ src/patternfly/patterns/NotificationDrawer/index.js
@@ -3,7 +3,11 @@ import docs from './docs.md'
 import './styles.scss'
 import Accordion, {AccordionItem} from '@components/Accordion'
 import Drawer from '@components/Drawer'
-import Split from '@layouts/Split'
+import MediaObject, {
+  MediaObjectLeft,
+  MediaObjectRight,
+  MediaObjectContent
+} from '@components/MediaObject'
 
 export const Docs = docs
 
@@ -13,10 +17,44 @@ export default ({children, className = ''}) => {
       <Drawer title="Notification Drawer">
         <Accordion>
           <AccordionItem title="Notification 1" className="pf-is-expanded">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius quod id delectus odio assumenda dolorem sunt, distinctio expedita commodi aliquam eaque ex minima cumque, nulla explicabo, repellendus autem, facilis iste.</p>
+            <MediaObject>
+              <MediaObjectLeft>
+                <i className="fas fa-info-circle"></i>
+              </MediaObjectLeft>
+              <MediaObjectContent>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              </MediaObjectContent>
+              <MediaObjectRight>
+                <i className="fas fa-ellipsis-v"></i>
+              </MediaObjectRight>
+            </MediaObject>
           </AccordionItem>
-          <AccordionItem title="Notification 2"></AccordionItem>
-          <AccordionItem title="Notification 3"></AccordionItem>
+          <AccordionItem title="Notification 2">
+            <MediaObject>
+              <MediaObjectLeft>
+                <i className="fas fa-info-circle"></i>
+              </MediaObjectLeft>
+              <MediaObjectContent>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              </MediaObjectContent>
+              <MediaObjectRight>
+                <i className="fas fa-ellipsis-v"></i>
+              </MediaObjectRight>
+            </MediaObject>
+          </AccordionItem>
+          <AccordionItem title="Notification 3">
+            <MediaObject>
+              <MediaObjectLeft>
+                <i className="fas fa-info-circle"></i>
+              </MediaObjectLeft>
+              <MediaObjectContent>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              </MediaObjectContent>
+              <MediaObjectRight>
+                <i className="fas fa-ellipsis-v"></i>
+              </MediaObjectRight>
+            </MediaObject>
+          </AccordionItem>
         </Accordion>
       </Drawer>
     </div>
