348fbbee6ce8b1a207f89ea3cfd4929cbb63d2fa
Matthew Carleton <mcarleton@redhat.com>
2018-03-13 18:48:21 -0300

    updates html

--- src/pages/components/secondary-nav/index.js
+++ src/pages/components/secondary-nav/index.js
@@ -6,21 +6,21 @@ import SecondaryNav, {SecondaryNavItem, Docs} from '@components/SecondaryNav'
 export default () => {
   return (
     <Documentation docs={Docs}>
-    <ul className="pf-c-secondary-nav" role="tablist">
+    <ul className="pf-c-secondary-nav">
     <li className="pf-c-secondary-nav__item">
       <a href="#" role="tab" className="pf-c-secondary-nav__link pf-is-active" aria-selected="true">Secondary Tab One</a>
     </li>
     <li className="pf-c-secondary-nav__item">
-      <a href="#" role="tab" className="pf-c-secondary-nav__link">Secondary Tab Two</a>
+      <a href="#" className="pf-c-secondary-nav__link">Secondary Tab Two</a>
     </li>
     <li className="pf-c-secondary-nav__item">
-      <a href="#" role="tab" className="pf-c-secondary-nav__link">Secondary Tab Three</a>
+      <a href="#" className="pf-c-secondary-nav__link">Secondary Tab Three</a>
     </li>
     <li className="pf-c-secondary-nav__item">
-      <a href="#" role="tab" className="pf-c-secondary-nav__link pf-is-disabled" aria-disabled="true">Secondary Tab Disabled</a>
+      <a href="#" className="pf-c-secondary-nav__link pf-is-disabled" aria-disabled="true">Secondary Tab Disabled</a>
     </li>
     <li className="pf-c-secondary-nav__item">
-      <a href="#" role="tab" className="pf-c-secondary-nav__link">Secondary Tab Five</a>
+      <a href="#" className="pf-c-secondary-nav__link">Secondary Tab Five</a>
     </li>
   </ul>
     </Documentation>
