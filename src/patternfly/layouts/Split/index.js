3cb43d477350be87c89090f1a429295498a9cf15
Cliff Pyles <cliffpyles@gmail.com>
2018-03-23 12:39:29 -0400

    Unify split layout with conventions (#81)
    * Update Split layout documentation and selectors
    
    * Add content to Split layout example

--- src/patternfly/layouts/Split/index.js
+++ src/patternfly/layouts/Split/index.js
@@ -4,6 +4,22 @@ import './styles.scss'
 
 export const Docs = docs
 
+export const SplitPrimary = ({children}) => {
+  return (
+    <div className="pf-l-split__primary">
+      {children}
+    </div>
+  )
+}
+
+export const SplitSecondary = ({children}) => {
+  return (
+    <div className="pf-l-split__secondary">
+      {children}
+    </div>
+  )
+}
+
 export default ({children, className = ''}) => {
   return (
     <div className={`pf-l-split ${className}`}>
