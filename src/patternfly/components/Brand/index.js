c1c47c0afa5574ea1c4cb55362a27d696262432f
Doug Miller <dmiller9911@gmail.com>
2018-03-27 13:59:23 -0400

    Add Brand Component

--- src/patternfly/components/Brand/index.js
+++ src/patternfly/components/Brand/index.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import docs from './docs.md'
-import './styles.scss'
+import React from 'react';
+import docs from './docs.md';
+import './styles.scss';
 
-export const Docs = docs
-
-var __html = require('./component.html');
-var template = { __html: __html };
+export const Docs = docs;
 
 export default () => {
-    return (
-        <div dangerouslySetInnerHTML={template} ></div>
-    )
-}
+  return (
+    <div style={{ background: '#000' }}>
+      <span className="pf-c-brand">PATTERNFLY NEXT</span>
+    </div>
+  );
+};
