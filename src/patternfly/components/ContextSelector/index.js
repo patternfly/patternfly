1961a1c5cd0bfe58c65860fa18cc8b21b35d52eb
Adam Jolicoeur <mindreeper2420@gmail.com>
2018-03-27 09:26:37 -0400

    Add Brand and ContextSelector components

---
+++ src/patternfly/components/ContextSelector/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import docs from './docs.md'
+import './styles.scss'
+
+export const Docs = docs
+
+var __html = require('./component.html');
+var template = { __html: __html };
+
+export default () => {
+    return (
+        <div dangerouslySetInnerHTML={template} style={{background: '#000'}}></div>
+    )
+}
