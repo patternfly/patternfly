dd787765bbbcee8c9c7d966b47f7f7da4bc15723
Mike Ro <mikerodev@gmail.com>
2018-03-27 09:58:33 -0400

    Refactor to include ToolbarGroup under Toolbar

--- src/pages/layouts/toolbar/index.js
+++ src/pages/layouts/toolbar/index.js
@@ -1,29 +1,27 @@
-import React from 'react'
-import Documentation from '@siteComponents/Documentation'
-import Example from '@siteComponents/Example'
-import Toolbar, {Docs} from '@layouts/Toolbar'
-import ToolbarGroup from "../../../patternfly/layouts/ToolbarGroup";
-import Button from '@components/Button'
-
+import React from 'react';
+import Documentation from '@siteComponents/Documentation';
+import Example from '@siteComponents/Example';
+import Toolbar, { ToolbarGroup, Docs } from '@layouts/Toolbar';
+import Button from '@components/Button';
 
 export default () => {
   return (
     <Documentation docs={Docs}>
       <Example heading="Toolbar Example">
         <Toolbar>
-            <ToolbarGroup>
-                <span>First child</span>
-                <span>Second child</span>
-            </ToolbarGroup>
-            <ToolbarGroup>
-                <Button className="pf-is-primary">Button</Button>
-                <Button className="pf-is-tertiary">Button</Button>
-            </ToolbarGroup>
-            <ToolbarGroup>
-                <Button className="pf-is-primary">k</Button>
-            </ToolbarGroup>
+          <ToolbarGroup>
+            <span>First child</span>
+            <span>Second child</span>
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <Button className="pf-is-primary">Button</Button>
+            <Button className="pf-is-tertiary">Button</Button>
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <Button className="pf-is-primary">k</Button>
+          </ToolbarGroup>
         </Toolbar>
       </Example>
     </Documentation>
-  )
-}
+  );
+};
--- src/patternfly/layouts/Toolbar/index.js
+++ src/patternfly/layouts/Toolbar/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import docs from './docs.md'
-import './styles.scss'
+import React from 'react';
+import docs from './docs.md';
+import './styles.scss';
 
-export const Docs = docs
+export const Docs = docs;
 
-export default ({children, className = ''}) => {
-  return (
-    <div className={`pf-l-toolbar ${className}`}>
-      {children}
-    </div>
-  )
-}
+export default ({ children, className = '' }) => {
+  return <div className={`pf-l-toolbar ${className}`}>{children}</div>;
+};
+
+export const ToolbarGroup = ({ children, className = '' }) => {
+  return <div className={`pf-l-toolbar__group ${className}`}>{children}</div>;
+};
