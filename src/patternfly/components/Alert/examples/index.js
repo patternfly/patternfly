0a44d95d7a55d37a2e6ec83b88be3c5a31a23bf4
Dana Gutride <dgutride@redhat.com>
2018-04-12 22:06:21 -0400

    Convert alert example to handlebars

--- src/patternfly/components/Alert/examples/index.js
+++ src/patternfly/components/Alert/examples/index.js
@@ -1,36 +1,35 @@
 import React from 'react'
 import Documentation from '@siteComponents/Documentation'
 import Example from '@siteComponents/Example'
-import Alert, { Docs } from '@components/Alert'
+import SuccessAlert from './success-alert.hbs'
+import WarningAlert from './warning-alert.hbs'
+import DangerAlert from './danger-alert.hbs'
+import InfoAlert from './info-alert.hbs'
+import docs from '../docs/code.md'
+import '../styles.scss'
+
+export const Docs = docs
 
 export default () => {
+  const successAlert = SuccessAlert()
+  const warningAlert = WarningAlert()
+  const dangerAlert = DangerAlert()
+  const infoAlert = InfoAlert()
+
   return (
     <Documentation docs={Docs}>
-
       <Example heading="Success Alert">
-        <Alert className="pf-is-success">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </Alert>
+        {successAlert}
       </Example>
-
       <Example heading="Danger Alert">
-        <Alert className="pf-is-danger">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </Alert>
+        {dangerAlert}
       </Example>
-
       <Example heading="Warning Alert">
-        <Alert className="pf-is-warning">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </Alert>
+        {warningAlert}
       </Example>
-
       <Example heading="Info Alert">
-        <Alert className="pf-is-info">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </Alert>
+        {infoAlert}
       </Example>
-
     </Documentation>
   )
 }
