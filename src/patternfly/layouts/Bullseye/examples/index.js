85d7837530c8680bd4c4c60d8090ce10b8299293
Dana Gutride <dgutride@gmail.com>
2018-04-13 09:44:00 -0400

    Convert bullseye layout to handlebars (#148)
    * Convert bullseye layout to handlebars
    
    * Change className to modifierClass for clarity

--- src/patternfly/layouts/Bullseye/examples/index.js
+++ src/patternfly/layouts/Bullseye/examples/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import Documentation from '@siteComponents/Documentation'
 import Example from '@siteComponents/Example'
-import Bullseye, {BullseyeItem, Docs} from '@layouts/Bullseye'
+import BullseyeTemplate from './bullseye-template.hbs'
+import docs from '../docs/code.md'
+import '../styles.scss'
+
+export const Docs = docs
 
 export default () => {
+  const bullseyeTemplate = BullseyeTemplate()
+
   return (
     <Documentation docs={Docs} className="is-layout-page">
       <Example heading="Bullseye Example">
-        <Bullseye>
-          <BullseyeItem></BullseyeItem>
-        </Bullseye>
+        {bullseyeTemplate}
       </Example>
     </Documentation>
   )
