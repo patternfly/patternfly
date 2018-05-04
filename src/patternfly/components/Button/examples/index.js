623cf387d394ad59f0cacfdee54b2bcf9cf58449
Sarah Rambacher <srambach@redhat.com>
2018-05-04 09:32:20 -0400

    Breaks alternate secondary button into dark preview

--- src/patternfly/components/Button/examples/index.js
+++ src/patternfly/components/Button/examples/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import ButtonTypesTemplate from './buttonTypes.hbs';
+import ButtonTypesTemplateAlt from './buttonTypesAlt.hbs';
 import ButtonStatesTemplate from './buttonStates.hbs';
+import ButtonStatesTemplateAlt from './buttonStatesAlt.hbs';
 import ButtonBlockTemplate from './buttonBlock.hbs';
 import docs from '../docs/code.md';
 import '../styles.scss';
@@ -11,13 +13,17 @@ export const Docs = docs;
 
 export default () => {
   const buttonTypesTemplate = ButtonTypesTemplate();
+  const buttonTypesTemplateAlt = ButtonTypesTemplateAlt();
   const buttonStatesTemplate = ButtonStatesTemplate();
+  const buttonStatesTemplateAlt = ButtonStatesTemplateAlt();
   const buttonBlockTemplate = ButtonBlockTemplate();
 
   return (
     <Documentation docs={Docs}>
       <Example heading="Button Types">{buttonTypesTemplate}</Example>
+      <Example heading="Button Types - alternate secondary"  className="is-dark-preview">{buttonTypesTemplateAlt}</Example>
       <Example heading="Button States">{buttonStatesTemplate}</Example>
+      <Example heading="Button States - alternate secondary" className="is-dark-preview">{buttonStatesTemplateAlt}</Example>
       <Example heading="Button (Block Level)">{buttonBlockTemplate}</Example>
     </Documentation>
   );
