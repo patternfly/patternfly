28d2085d54645d609e2518aa3321ab88349198b9
Jenn Giardino <jgiardin@redhat.com>
2018-05-11 11:22:24 -0400

    Finalize values for line height variables (#204)

--- src/patternfly/components/Content/examples/index.js
+++ src/patternfly/components/Content/examples/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import ContentTemplate from './content-template.hbs';
-import ContentVisualDesignTemplate from './content-visual-design-template.hbs';
 import docs from '../docs/code.md';
 import '../styles.scss';
 
@@ -10,14 +9,10 @@ export const Docs = docs;
 
 export default () => {
   const contentTemplate = ContentTemplate();
-  const contentVisualDesignTemplate = ContentVisualDesignTemplate();
 
   return (
     <Documentation docs={Docs} className="">
       <Example heading="Content Example">{contentTemplate}</Example>
-      <Example heading="This is here for visual design review purposes only and will be deleted">
-        {contentVisualDesignTemplate}
-      </Example>
     </Documentation>
   );
 };
