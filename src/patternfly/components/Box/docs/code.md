30e4f42c8f7d890e2b2446571b1436c628fcefbe
Sarah Rambacher <srambach@redhat.com>
2018-05-18 09:25:51 -0400

    Adds fullbleed option to a box

--- src/patternfly/components/Box/docs/code.md
+++ src/patternfly/components/Box/docs/code.md
@@ -7,3 +7,4 @@ A box is a generic rectangular container that can be used to build other compone
 | Class | Applied | Outcome |
 | -- | -- | -- |
 | `.pf-c-box` | `<div>` | Creates a box containing content |
+| `.pf-is-fullbleed` | `.pf-c-box` | Removes padding on the box so that the contents extend to the edge of the box. The box's child elements will be padded to ensure proper spacing to the outside of the box. ** Note: ** All direct children of a full bleed box should be block-level elements. |
--- src/patternfly/components/Box/examples/index.js
+++ src/patternfly/components/Box/examples/index.js
@@ -2,17 +2,21 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import docs from '../docs/code.md';
-import Example1 from './example1.hbs';
+import Example1 from './example-basic.hbs';
+import Example2 from './example-fullbleed.hbs';
 import '../styles.scss';
 
 export const Docs = docs;
 
 export default () => {
   const example1 = Example1();
+  const example2 = Example2();
 
   return (
     <Documentation docs={Docs}>
       <Example heading="Box">{example1}</Example>
+      <Example heading="Box - Full bleed">{example2}</Example>
     </Documentation>
+
   );
 };
