250d8df576eb8b115d943bbe26df8219c1b75abd
Sarah Rambacher <srambach@redhat.com>
2018-05-23 11:56:35 -0400

    Fixes naming of box files

--- src/patternfly/components/Box/examples/index.js
+++ src/patternfly/components/Box/examples/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import Documentation from '@siteComponents/Documentation';
-import Example from '@siteComponents/Example';
-import docs from '../docs/code.md';
-import Example1 from './example-basic.hbs';
-import Example2 from './example-fullbleed.hbs';
-import '../styles.scss';
+import React from "react";
+import Documentation from "@siteComponents/Documentation";
+import Example from "@siteComponents/Example";
+import docs from "../docs/code.md";
+import BoxBasicExample from "./box-basic-example.hbs";
+import BoxFullBleedExample from "./box-fullbleed-example.hbs";
+import "../styles.scss";
 
 export const Docs = docs;
 
 export default () => {
-  const example1 = Example1();
-  const example2 = Example2();
+  const boxBasicExample = BoxBasicExample();
+  const boxFullBleedExample = BoxFullBleedExample();
 
   return (
     <Documentation docs={Docs}>
-      <Example heading="Box">{example1}</Example>
-      <Example heading="Box - Full bleed">{example2}</Example>
+      <Example heading="Box">{boxBasicExample}</Example>
+      <Example heading="Box - Full bleed">{boxFullBleedExample}</Example>
     </Documentation>
-
   );
 };
