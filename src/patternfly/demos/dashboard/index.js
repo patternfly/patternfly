189811f961ecb78c5c437b2ad169e60f1e995367
Dana Gutride <dgutride@gmail.com>
2018-05-25 15:01:35 -0400

    chore(demos): Move demos into patternfly directory and update blueprint (#246)

--- src/patternfly/demos/dashboard/index.js
+++ src/patternfly/demos/dashboard/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import './styles.scss';
-import Template from './template.hbs';
 import Example from '@siteComponents/Example';
 import Documentation from '@siteComponents/Documentation';
+import Template from './template.hbs';
+import './styles.scss';
 
 export default () => {
   const template = Template();
 
   return (
     <Documentation>
-      <Example className="pf-d-dashboard" heading="Dashboard Demo">{template}</Example>
+      <Example className="pf-d-dashboard" heading="Dashboard Demo">
+        {template}
+      </Example>
     </Documentation>
   );
 };
