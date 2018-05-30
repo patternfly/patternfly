6de02d13edcd5e73d70610015961d3424aad6e37
Dana Gutride <dgutride@redhat.com>
2018-05-30 15:12:23 -0400

    fix(fonts): add fonts to demo page layouts

--- src/site/layouts/demo.js
+++ src/site/layouts/demo.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import Prism from 'prismjs'; // eslint-disable-line no-unused-vars
 import '../workspace.scss';
 
-export default ({ children }) => <div>{children()}</div>;
+export default ({ children }) => (
+  <div>
+    <Helmet>
+      <title>PF Next</title>
+      <script defer src="//use.fontawesome.com/releases/v5.0.8/js/all.js" />
+    </Helmet>
+    <div>{children()}</div>
+  </div>
+);
