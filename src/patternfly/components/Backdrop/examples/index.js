a2fb83bce70882c02b92f5cdca0e6206de1cf53f
Sarah Rambacher <srambach@redhat.com>
2018-06-01 13:01:57 -0400

    feat(Backdrop): Implements backdrop (#255)

---
+++ src/patternfly/components/Backdrop/examples/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Documentation from '@siteComponents/Documentation';
+import Example from '@siteComponents/Example';
+import docs from '../docs/code.md';
+import BackdropExample from './backdrop-example.hbs';
+import '../styles.scss';
+
+export const Docs = docs;
+
+export default () => {
+  const backdropExample = BackdropExample();
+
+  return (
+    <Documentation docs={Docs}>
+      <Example heading="Backdrop Example">{backdropExample}</Example>
+    </Documentation>
+  );
+};
