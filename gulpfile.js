81eca5d29ad41c70f15702f14b5ddc5ab1b26c98
Dana Gutride <dgutride@redhat.com>
2018-06-08 11:00:22 -0400

    fix(package): add basic and full css output for patternfly

--- gulpfile.js
+++ gulpfile.js
@@ -22,16 +22,25 @@ gulp.task('copy-fa', () =>
 gulp.task('build-tmp', () =>
   gulp
     .src('./src/patternfly/**/*.scss')
-    .pipe(insert.append('@import "./components/**/*.scss";\n'))
-    .pipe(insert.append('@import "./layouts/**/*.scss";\n'))
     .pipe(sassGlob())
     .pipe(replace('@import "../../patternfly-utilities";', ''))
     .pipe(gulp.dest('./tmp'))
 );
 
+gulp.task('build-tmp-full', () =>
+  gulp
+    .src('./src/patternfly/**/patternfly.scss')
+    .pipe(insert.append('@import "./components/**/*.scss";\n'))
+    .pipe(insert.append('@import "./layouts/**/*.scss";\n'))
+    .pipe(sassGlob())
+    .pipe(replace('@import "../../patternfly-utilities";', ''))
+    .pipe(rename('patternfly-full.scss'))
+    .pipe(gulp.dest('./tmp'))
+);
+
 gulp.task('build-library', ['build-tmp'], () =>
   gulp
-    .src('./tmp/patternfly.scss')
+    .src('./tmp/patternfly*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'))
 );
@@ -43,7 +52,7 @@ gulp.task('build-modules', () =>
     .pipe(gulp.dest('./dist'))
 );
 
-gulp.task('copy-source', ['build-tmp'], () => {
+gulp.task('copy-source', ['build-tmp-full', 'build-tmp'], () => {
   gulp.src('./README.md').pipe(gulp.dest('./dist'));
   gulp.src('./package.json').pipe(gulp.dest('./dist'));
   gulp.src('./tmp/**/*.scss').pipe(gulp.dest('./dist'));
