519badf07005ae20accc8cb4e6b4598b2483e9f8
Matt Nolting <matthew.nolting@gmail.com>
2018-06-08 15:45:57 -0400

    feat(form demo): create basic form demo (#319)
    * Generate simple form demo
    
    * Rename files
    
    * Update partial calls
    
    * Adding new files
    
    * Expands documentation
    
    * Removes pointer on input hover

---
+++ src/patternfly/demos/SimpleFormDemo/examples/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Documentation from '@siteComponents/Documentation';
+import Example from '@siteComponents/Example';
+import docs from '../docs/code.md';
+import SimpleFormDemoExample from './simple-form-demo-example.hbs';
+import '../styles.scss';
+
+export const Docs = docs;
+
+export default () => {
+  const simpleFormDemoExample = SimpleFormDemoExample();
+
+  return (
+    <Documentation docs={Docs}>
+      <Example heading="Simple Form Demo Example">
+        {simpleFormDemoExample}
+      </Example>
+    </Documentation>
+  );
+};
