ff111c7b9ad6571a5967bf933e9686d97fc71787
Matt Nolting <mnolting@redhat.com>
2018-06-25 11:14:30 -0400

    Responding to @andresgalante review feedback

--- src/patternfly/layouts/Toolbar/examples/index.js
+++ src/patternfly/layouts/Toolbar/examples/index.js
@@ -3,12 +3,10 @@ import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import ToolbarBasicExampleRaw from '!raw!./toolbar-basic-example.hbs';
 import ToolbarGroupAlignmentExampleRaw from '!raw!./toolbar-group-alignment-example.hbs';
-import ToolbarItemAlignmentExampleRaw from '!raw!./toolbar-item-alignment-example.hbs';
 import ToolbarSectionExampleRaw from '!raw!./toolbar-section-example.hbs';
 import docs from '../docs/code.md';
 import ToolbarBasicExample from './toolbar-basic-example.hbs';
 import ToolbarGroupAlignmentExample from './toolbar-group-alignment-example.hbs';
-import ToolbarItemAlignmentExample from './toolbar-item-alignment-example.hbs';
 import ToolbarSectionExample from './toolbar-section-example.hbs';
 import '../styles.scss';
 
@@ -17,7 +15,6 @@ export const Docs = docs;
 export default () => {
   const toolbarBasicExample = ToolbarBasicExample();
   const toolbarGroupAlignmentExample = ToolbarGroupAlignmentExample();
-  const toolbarItemAlignmentExample = ToolbarItemAlignmentExample();
   const toolbarSectionExample = ToolbarSectionExample();
 
   return (
@@ -33,12 +30,6 @@ export default () => {
         handlebars={ToolbarGroupAlignmentExampleRaw}
       >
         {toolbarGroupAlignmentExample}
-      </Example>
-      <Example
-        heading="Toolbar Item Alignment Example (responsive modifiers included)"
-        handlebars={ToolbarItemAlignmentExampleRaw}
-      >
-        {toolbarItemAlignmentExample}
       </Example>
       <Example
         heading="Toolbar Section Example (responsive modifiers included)"
