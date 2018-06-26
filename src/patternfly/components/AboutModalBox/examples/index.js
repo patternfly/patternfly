2ef3fa1365bb3e6f41af5bd94a7480e9001f8cb5
Matt Nolting <mnolting@redhat.com>
2018-06-26 11:55:35 -0400

    Add handlebars raw, responding to @mceledonia feedback - button, strapline, logo updates

--- src/patternfly/components/AboutModalBox/examples/index.js
+++ src/patternfly/components/AboutModalBox/examples/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
+import AboutModalBoxExampleRaw from '!raw!./about-modal-box-example.hbs';
 import docs from '../docs/code.md';
 import AboutModalBoxExample from './about-modal-box-example.hbs';
 import '../styles.scss';
@@ -12,7 +13,9 @@ export default () => {
 
   return (
     <Documentation docs={Docs}>
-      <Example heading="About modal box">{aboutModalBoxExample}</Example>
+      <Example heading="About modal box" handlebars={AboutModalBoxExampleRaw}>
+        {aboutModalBoxExample}
+      </Example>
     </Documentation>
   );
 };
