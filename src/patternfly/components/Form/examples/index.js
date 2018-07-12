9428eda7c7e2d20ab0271ea0d242272d1ca3a462
matthew carleton <mcarleton@redhat.com>
2018-07-12 13:51:37 -0300

    fix(form):update form with new components (#451)
    * fix(form):update form with new components
    
    * doc updates
    
    * fix handlebars
    
    * updates
    
    * form updates, input group updates

--- src/patternfly/components/Form/examples/index.js
+++ src/patternfly/components/Form/examples/index.js
@@ -2,29 +2,44 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import docs from '../docs/code.md';
+import FormTextInputExampleRaw from '!raw!./form-text-input-example.hbs';
 import FormTextInputExample from './form-text-input-example.hbs';
+import FormInputGroupExampleRaw from '!raw!./form-input-group-example.hbs';
+import FormInputGroupExample from './form-input-group-example.hbs';
+import FormSelectInputExampleRaw from '!raw!./form-select-input-example.hbs';
 import FormSelectInputExample from './form-select-input-example.hbs';
+import FormChecksRadioExampleRaw from '!raw!./form-check-radio-example.hbs';
 import FormChecksRadioExample from './form-check-radio-example.hbs';
+import FormTextareaExampleRaw from '!raw!./form-textarea-example.hbs';
 import FormTextareaExample from './form-textarea-example.hbs';
-import FormDatalistExample from './form-datalist-example.hbs';
 import '../styles.scss';
 
 export const Docs = docs;
 
 export default () => {
   const formTextInputExample = FormTextInputExample();
+  const formInputGroupExample = FormInputGroupExample();
   const formSelectInputExample = FormSelectInputExample();
   const formChecksRadioExample = FormChecksRadioExample();
   const formTextareaExample = FormTextareaExample();
-  const formDatalistExample = FormDatalistExample();
 
   return (
     <Documentation docs={Docs}>
-      <Example heading="Checkboxes/Radios">{formChecksRadioExample}</Example>
-      <Example heading="Text inputs">{formTextInputExample}</Example>
-      <Example heading="Select inputs">{formSelectInputExample}</Example>
-      <Example heading="Textareas">{formTextareaExample}</Example>
-      <Example heading="Datalist">{formDatalistExample}</Example>
+      <Example heading="Checkboxes/Radios" handlebars={FormChecksRadioExampleRaw}>
+      {formChecksRadioExample}
+      </Example>
+      <Example heading="Text inputs" handlebars={FormTextInputExampleRaw}>
+      {formTextInputExample}
+      </Example>
+      <Example heading="Input groups" handlebars={FormInputGroupExampleRaw}>
+      {formInputGroupExample}
+      </Example>
+      <Example heading="Select inputs" handlebars={FormSelectInputExampleRaw}>
+      {formSelectInputExample}
+      </Example>
+      <Example heading="Textareas" handlebars={FormTextareaExampleRaw}>
+      {formTextareaExample}
+      </Example>
     </Documentation>
   );
 };
