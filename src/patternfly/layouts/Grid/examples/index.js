9934d354be14843bfa2bee88afefc8ca9cc4fe93
Matt Nolting <mnolting@redhat.com>
2018-07-19 09:04:33 -0400

    Add smart grid declarations, add media-query to mixins, add sass maps to variables.scss

--- src/patternfly/layouts/Grid/examples/index.js
+++ src/patternfly/layouts/Grid/examples/index.js
@@ -3,12 +3,14 @@ import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import GridBaseRaw from '!raw!./grid-base-example.hbs';
 import GridGuttersRaw from '!raw!./grid-gutters-example.hbs';
+import GridSmartRaw from '!raw!./grid-smart-grid-example.hbs';
 import GridNestedRaw from '!raw!./grid-nested-example.hbs';
 import GridOffsetsRaw from '!raw!./grid-offsets-example.hbs';
 import GridResponsiveRaw from '!raw!./grid-responsive-example.hbs';
 import GridRowspanRaw from '!raw!./grid-rowspan-example.hbs';
 import GridBase from './grid-base-example.hbs';
 import GridGutters from './grid-gutters-example.hbs';
+import GridSmart from './grid-smart-grid-example.hbs';
 import GridNested from './grid-nested-example.hbs';
 import GridOffsets from './grid-offsets-example.hbs';
 import GridResponsive from './grid-responsive-example.hbs';
@@ -21,6 +23,7 @@ export const Docs = docs;
 export default () => {
   const gridBase = GridBase();
   const gridGutters = GridGutters();
+  const gridSmart = GridSmart();
   const gridNested = GridNested();
   const gridOffsets = GridOffsets();
   const gridResponsive = GridResponsive();
@@ -28,6 +31,9 @@ export default () => {
 
   return (
     <Documentation docs={Docs} className="is-layout-page">
+      <Example heading="Smart grid (responsive)" handlebars={GridSmartRaw}>
+        {gridSmart}
+      </Example>
       <Example heading="Base grid" handlebars={GridBaseRaw}>
         {gridBase}
       </Example>
