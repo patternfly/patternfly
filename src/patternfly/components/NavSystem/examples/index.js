e5eb00c80685109fa22b44689f67e0c73e35294c
Matt Nolting <mnolting@redhat.com>
2018-07-21 01:22:59 -0400

    Create nav-system structure

--- src/patternfly/components/NavSystem/examples/index.js
+++ src/patternfly/components/NavSystem/examples/index.js
@@ -1,27 +1,58 @@
 import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
-import navSystemSimpleExampleRaw from '!raw!./nav-system-simple-example.hbs';
-import navSystemComplexExampleRaw from '!raw!./nav-system-complex-example.hbs';
-import NavsystemSimpleExample from './nav-system-simple-example.hbs';
-import NavsystemComplexExample from './nav-system-complex-example.hbs';
-import navSystemComplexExampleDoc from '../docs/nav-system-complex.md';
+
+// Raw files
+import navListSimpleExampleRaw from '!raw!./nav-list-simple-example.hbs';
+import navListGroupedExampleRaw from '!raw!./nav-list-grouped-example.hbs';
+import navExampleRaw from '!raw!./nav-example.hbs';
+import navExpandableExampleRaw from '!raw!./nav-expandable-example.hbs';
+import navMixedExampleRaw from '!raw!./nav-mixed-example.hbs';
+import navListHorizontalExampleRaw from '!raw!./nav-list-horizontal-example.hbs';
+
+// Example files
+import NavListSimpleExample from './nav-list-simple-example.hbs';
+import NavListGroupedExample from './nav-list-grouped-example.hbs';
+import NavExample from './nav-example.hbs';
+import NavExpandableExample from './nav-expandable-example.hbs';
+import NavMixedExample from './nav-mixed-example.hbs';
+import NavListHorizontalExample from './nav-list-horizontal-example.hbs';
+
+// Docs
+import navExampleDoc from '../docs/nav.md';
+
 import docs from '../docs/code.md';
 import '../styles.scss';
 
 export const Docs = docs;
 
 export default () => {
-  const navSystemSimpleExample = NavsystemSimpleExample();
-  const navSystemComplexExample = NavsystemComplexExample();
+  const navListSimpleExample = NavListSimpleExample();
+  const navListGroupedExample = NavListGroupedExample();
+  const navExample = NavExample();
+  const navExpandableExample = NavExpandableExample();
+  const navMixedExample = NavMixedExample();
+  const navListHorizontalExample = NavListHorizontalExample();
 
   return (
     <Documentation docs={Docs}>
-      <Example heading="Navsystem Simple" handlebars={navSystemSimpleExampleRaw}>
-        {navSystemSimpleExample}
+      <Example heading="Nav List Simple" handlebars={navListSimpleExampleRaw}>
+        {navListSimpleExample}
       </Example>
-      <Example heading="Navsystem Complex" handlebars={navSystemComplexExampleRaw} docs={navSystemComplexExampleDoc}>
-        {navSystemComplexExample}
+      <Example heading="Nav List Grouped" handlebars={navListGroupedExampleRaw}>
+        {navListGroupedExample}
+      </Example>
+      <Example heading="Nav Default" handlebars={navExampleRaw} docs={navExampleDoc}>
+        {navExample}
+      </Example>
+      <Example heading="Nav Expandable" handlebars={navExpandableExampleRaw}>
+        {navExpandableExample}
+      </Example>
+      <Example heading="Nav Mixed" handlebars={navMixedExampleRaw}>
+        {navMixedExample}
+      </Example>
+      <Example heading="Nav List Horizontal" handlebars={navListHorizontalExampleRaw}>
+        {navListHorizontalExample}
       </Example>
     </Documentation>
   );
