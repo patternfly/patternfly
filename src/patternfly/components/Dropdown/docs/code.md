07b978cc3b6e784ad8da4301247791264dc127ff
Michael Coker <mcoker@redhat.com>
2018-08-07 15:27:17 -0500

    refactor(dropdown): change pf-m-right-aligned to pf-m-align-right

--- src/patternfly/components/Dropdown/docs/code.md
+++ src/patternfly/components/Dropdown/docs/code.md
@@ -32,5 +32,5 @@ The dropdown menu can contain either links or buttons, depending on the expected
 | `.pf-m-expanded` | `.pf-c-dropdown` | Modifies for the expanded state |
 | `.pf-m-action` | `.pf-c-dropdown` | Modifies to display the toggle with no border or background |
 | `.pf-m-dropup` | `.pf-c-dropdown` | Modifies to display the menu above the toggle |
-| `.pf-m-right-aligned` | `.pf-c-dropdown` | Modifies to display the menu right-aligned with the toggle |
+| `.pf-m-align-right` | `.pf-c-dropdown` | Modifies to display the menu aligned to the right edge of the toggle |
 | `.pf-m-disabled` | `.pf-c-dropdown__menu-item` | Modifies to display the menu item as disabled |
--- src/patternfly/components/Dropdown/examples/index.js
+++ src/patternfly/components/Dropdown/examples/index.js
@@ -4,14 +4,14 @@ import Example from '@siteComponents/Example';
 import DropdownExpandedRaw from '!raw!./dropdown-expanded-example.hbs';
 import DropdownCollapsedRaw from '!raw!./dropdown-collapsed-example.hbs';
 import DropdownKebabRaw from '!raw!./dropdown-kebab-example.hbs';
-import DropdownKebabRightAlignedRaw from '!raw!./dropdown-kebab-right-aligned-example.hbs';
-import DropdownRightAlignedRaw from '!raw!./dropdown-right-aligned-example.hbs';
+import DropdownKebabAlignRightRaw from '!raw!./dropdown-kebab-align-right-example.hbs';
+import DropdownAlignRightRaw from '!raw!./dropdown-align-right-example.hbs';
 import DropdownDropupRaw from '!raw!./dropdown-dropup-example.hbs';
 import DropdownExpanded from './dropdown-expanded-example.hbs';
 import DropdownCollapsed from './dropdown-collapsed-example.hbs';
 import DropdownKebab from './dropdown-kebab-example.hbs';
-import DropdownKebabRightAligned from './dropdown-kebab-right-aligned-example.hbs';
-import DropdownRightAligned from './dropdown-right-aligned-example.hbs';
+import DropdownKebabAlignRight from './dropdown-kebab-align-right-example.hbs';
+import DropdownAlignRight from './dropdown-align-right-example.hbs';
 import DropdownDropup from './dropdown-dropup-example.hbs';
 import docs from '../docs/code.md';
 import '../styles.scss';
@@ -22,8 +22,8 @@ export default () => {
   const dropdownExpanded = DropdownExpanded();
   const dropdownCollapsed = DropdownCollapsed();
   const dropdownKebab = DropdownKebab();
-  const dropdownKebabRightAligned = DropdownKebabRightAligned();
-  const dropdownRightAligned = DropdownRightAligned();
+  const dropdownKebabAlignRight = DropdownKebabAlignRight();
+  const dropdownAlignRight = DropdownAlignRight();
   const dropdownDropup = DropdownDropup();
 
   const styles = {
@@ -43,17 +43,17 @@ export default () => {
       </Example>
       <Example
         className="is-expanded-dropdown is-right-aligned"
-        heading="Kebab Right Aligned"
-        handlebars={DropdownKebabRightAlignedRaw}
+        heading="Kebab Align Right"
+        handlebars={DropdownKebabAlignRightRaw}
       >
-        {dropdownKebabRightAligned}
+        {dropdownKebabAlignRight}
       </Example>
       <Example
         className="is-expanded-dropdown is-right-aligned"
-        heading="Right Aligned"
-        handlebars={DropdownRightAlignedRaw}
+        heading="Align Right"
+        handlebars={DropdownAlignRightRaw}
       >
-        {dropdownRightAligned}
+        {dropdownAlignRight}
       </Example>
       <Example className="is-expanded-dropup" heading="Dropup" handlebars={DropdownDropupRaw}>
         {dropdownDropup}
