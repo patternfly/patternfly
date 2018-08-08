6c19e057addd4758b18155ad3b8b26ef88d0ae53
Michael Coker <mcoker@redhat.com>
2018-08-08 16:17:10 -0500

    refactor(split, stack): change pf-m-primary to pf-m-main and drop unnecessary modifiers

--- src/patternfly/layouts/Split/examples/index.js
+++ src/patternfly/layouts/Split/examples/index.js
@@ -3,10 +3,8 @@ import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import SplitRaw from '!raw!./split-example.hbs';
 import SplitGuttersRaw from '!raw!./split-gutters-example.hbs';
-import SplitNoPrimarySecondaryRaw from '!raw!./split-noprimarysecondary-example.hbs';
 import Split from './split-example.hbs';
 import SplitGutters from './split-gutters-example.hbs';
-import SplitNoPrimarySecondary from './split-noprimarysecondary-example.hbs';
 import docs from '../docs/code.md';
 import '../styles.scss';
 
@@ -15,7 +13,6 @@ export const Docs = docs;
 export default () => {
   const split = Split();
   const splitGutters = SplitGutters();
-  const splitNoPrimarySecondary = SplitNoPrimarySecondary();
   return (
     <Documentation docs={Docs} className="is-layout-page">
       <Example heading="Split Example" handlebars={SplitRaw}>
@@ -23,12 +20,6 @@ export default () => {
       </Example>
       <Example heading="Split Example with gutter" handlebars={SplitGuttersRaw}>
         {splitGutters}
-      </Example>
-      <Example
-        heading="Split Example with no primary or secondary children specified"
-        handlebars={SplitNoPrimarySecondaryRaw}
-      >
-        {splitNoPrimarySecondary}
       </Example>
     </Documentation>
   );
