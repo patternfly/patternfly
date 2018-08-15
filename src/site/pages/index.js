d91440cb180c3aaf0afcc99a030d0f374a211ab8
Dana Gutride <dgutride@redhat.com>
2018-08-15 15:40:15 -0400

    fix(link): add pdf link to index.js and regenerate modifiers

--- src/site/pages/index.js
+++ src/site/pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import quickRefPdf from './reference-docs/PF-quick-ref.pdf';
 
 export default () => (
   <div>
-    <h1 class="layout__content_heading">Patternfly Workspace</h1>
-    <p>Just getting started? Take a look at the <a href="/contribution">Contribution Guide</a> and <a href="/guidelines">Coding Guidelines.</a></p>
-    <p>Always keep our <a href="/accessibility-guide">Accessibility Guide</a> in mind.</p>
-    <p>Download a <a href="/PF-quick-ref.pdf">quick reference sheet.</a></p>
+    <h1 className="layout__content_heading">Patternfly Workspace</h1>
+    <p>
+      Just getting started? Take a look at the <a href="/contribution">Contribution Guide</a> and{' '}
+      <a href="/guidelines">Coding Guidelines.</a>
+    </p>
+    <p>
+      Always keep our <a href="/accessibility-guide">Accessibility Guide</a> in mind.
+    </p>
+    <p>
+      Download a <a href={quickRefPdf}>quick reference sheet.</a>
+    </p>
   </div>
 );
