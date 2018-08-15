0c8e834c95aba3880f0f6860236f074e815b323e
Sarah Rambacher <srambach@redhat.com>
2018-08-15 15:11:12 -0400

    feat(quickreference): Adds a quick reference guide for CSS (#594)
    * feat(quickreference): Adds a quick reference guide for CSS
    
    * Updates modifiers, adds spacers, adds placeholder link to reference

--- src/site/pages/index.js
+++ src/site/pages/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default () => (
   <div>
-    <h1>Home</h1>
+    <h1 class="layout__content_heading">Patternfly Workspace</h1>
+    <p>Just getting started? Take a look at the <a href="/contribution">Contribution Guide</a> and <a href="/guidelines">Coding Guidelines.</a></p>
+    <p>Always keep our <a href="/accessibility-guide">Accessibility Guide</a> in mind.</p>
+    <p>Download a <a href="/PF-quick-ref.pdf">quick reference sheet.</a></p>
   </div>
 );
