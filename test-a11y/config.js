97b2d47070da315009f68f1facf42ed2436b4e79
Matthew Carleton <mcarleton@redhat.com>
2018-08-29 10:54:03 -0300

    fix(a11y):update config script

--- test-a11y/config.js
+++ test-a11y/config.js
@@ -1,8 +1,5 @@
 /* eslint global-require: 0, import/no-dynamic-require: 0 */
-const path = require('path');
-const program = require('commander');
-
-const defaultConfig = {
+module.exports = {
   toleranceThreshold: 14,
   host: 'localhost',
   port: '8000',
@@ -14,25 +11,4 @@ const defaultConfig = {
     yellow: '\x1b[33m',
     reset: '\x1b[0m'
   }
-};
-let incomingConfig = {};
-let config = {};
-
-program.option('-c, --config <path>', 'specifies a user defined configuration').parse(process.argv);
-
-if (program.config) {
-  const configPath = path.resolve(process.cwd(), program.config);
-  incomingConfig = require(configPath);
-  config = {
-    ...defaultConfig,
-    ...incomingConfig
-  };
-} else {
-  config = {
-    ...defaultConfig
-  };
-}
-
-module.exports = {
-  ...config
 };
