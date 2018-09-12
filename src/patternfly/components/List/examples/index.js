7065216ed0f3c14d32ef5dba3c1498a5a6b1fe86
Andy Braren <andybraren@users.noreply.github.com>
2018-09-12 08:16:46 -0400

    feat(list): add list component (#711)
    * feat(list): add list component
    
    Introduces the list component with two modifiers (inline and grid) that change the presentation of list items. If the list component is used without modifiers, it gets the same styling as lists within the content component.
    
    The inline modifier displays the list items in a horizontal line using flexbox. The grid modifier positions list items within a flexible grid with a minimum column width of 100px.
    
    * Clarified pf-c-list class behavior
    
    * Commented on usage of type selectors
    
    * Updated pf-m-inline wrapping behavior
    
    Inline lists now wrap appropriately instead of not wrapping at all.
    
    * Fixed modifier and attribute syntax
    
    Demos updated accordingly.

---
+++ src/patternfly/components/List/examples/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Documentation from '@siteComponents/Documentation';
+import Example from '@siteComponents/Example';
+import listSimpleExampleRaw from '!raw!./list-simple-example.hbs';
+import listInlineExampleRaw from '!raw!./list-inline-example.hbs';
+import listGridExampleRaw from '!raw!./list-grid-example.hbs';
+import ListSimpleExample from './list-simple-example.hbs';
+import ListInlineExample from './list-inline-example.hbs';
+import ListGridExample from './list-grid-example.hbs';
+import docs from '../docs/code.md';
+import '../styles.scss';
+
+export const Docs = docs;
+
+export default () => {
+  const listSimpleExample = ListSimpleExample();
+  const listInlineExample = ListInlineExample();
+  const listGridExample = ListGridExample();
+
+  return (
+    <Documentation docs={Docs}>
+      <Example heading="List" handlebars={listSimpleExampleRaw}>
+        {listSimpleExample}
+      </Example>
+      <Example heading="List Inline" handlebars={listInlineExampleRaw}>
+        {listInlineExample}
+      </Example>
+      <Example heading="List Grid" handlebars={listGridExampleRaw}>
+        {listGridExample}
+      </Example>
+    </Documentation>
+  );
+};
