e781a9e41cfd1c73b6171043b18555cf935c6731
Christie Molloy <cmolloy@bu.edu>
2018-09-13 08:21:56 -0400

    Alert Component/Accessibility&Alignment Utilities -- html converted to hbs (#719)
    * alert component changed from html to hbs
    
    * accessibility and alignmenthtml to hbs
    
    * matts changes
    
    * 2/5 changes made
    
    * matts changes round 2, aria label not working
    
    * changes made
    
    * michaels changes
    
    * accessibility in hbs
    
    * alignment converted to hbs
    
    * capitalized L

---
+++ src/patternfly/components/Alert/alert-infocircle-icon.hbs
@@ -0,0 +1,1 @@
+<i class="fas fa-info-circle" aria-hidden="true"></i>
--- src/patternfly/utilities/Accessibility/examples/index.js
+++ src/patternfly/utilities/Accessibility/examples/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import srOnlyExampleRaw from '!raw!./accessibility-sr-only-example.hbs';
+import visibleExampleRaw from '!raw!./accessibility-visible-example.hbs';
+import hiddenExampleRaw from '!raw!./accessibility-hidden-example.hbs';
 import SrOnlyExample from './accessibility-sr-only-example.hbs';
+import VisibleExample from './accessibility-visible-example.hbs';
+import HiddenExample from './accessibility-hidden-example.hbs';
 import docs from '../docs/code.md';
 import '../styles.scss';
 
@@ -10,12 +14,20 @@ export const Docs = docs;
 
 export default () => {
   const srOnlyExample = SrOnlyExample();
+  const visibleExample = VisibleExample();
+  const hiddenExample = HiddenExample();
   const headingText = 'Accessibility';
 
   return (
     <Documentation docs={Docs} heading={headingText} className="is-utility-page">
       <Example heading="Screen reader only" handlebars={srOnlyExampleRaw}>
         {srOnlyExample}
+      </Example>
+      <Example heading="Visible" handlebars={visibleExampleRaw}>
+        {visibleExample}
+      </Example>
+      <Example heading="Hidden" handlebars={hiddenExampleRaw}>
+        {hiddenExample}
       </Example>
     </Documentation>
   );
