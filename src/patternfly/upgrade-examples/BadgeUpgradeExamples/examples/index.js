6171c6dc51f4194dcf7bee77b03f3e6f6721ee32
Christie Molloy <cmolloy@cmolloy-mac.local>
2018-09-17 15:25:57 -0400

    badge upgrade examples

--- src/patternfly/upgrade-examples/BadgeUpgradeExamples/examples/index.js
+++ src/patternfly/upgrade-examples/BadgeUpgradeExamples/examples/index.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
+import badgeUpgradeExamplespf3Raw from '!raw!./badge-upgrade-examples-pf3.hbs';
+import badgeUpgradeExamplespf4Raw from '!raw!./badge-upgrade-examples-pf4.hbs';
+import Badgeupgradeexamplespf3 from './badge-upgrade-examples-pf3.hbs';
+import Badgeupgradeexamplespf4 from './badge-upgrade-examples-pf4.hbs';
+import badgeUpgradeExamplesComplexExampleDoc from '../docs/badge-upgrade-examples-complex.md';
 import docs from '../docs/code.md';
-import CardupgradeexamplesExample1 from './card-upgrade-examples-example1.hbs';
-import CardupgradeexamplesExample2 from './card-upgrade-examples-example2.hbs';
 import '../styles.scss';
 
 export const Docs = docs;
 
 export default () => {
-  const cardUpgradeExamplesExample1 = CardupgradeexamplesExample1();
-  const cardUpgradeExamplesExample2 = CardupgradeexamplesExample2();
-  const headingText = 'Card Migration';
+  const badgeUpgradeExamplespf3 = Badgeupgradeexamplespf3();
+  const badgeUpgradeExamplespf4 = Badgeupgradeexamplespf4();
 
   return (
-    <Documentation docs={Docs} heading={headingText}>
-      <Example heading="PatternFly 3 Cards">{cardUpgradeExamplesExample1}</Example>
-      <Example heading="PatternFly 4 Cards">{cardUpgradeExamplesExample2}</Example>
+    <Documentation docs={Docs}>
+      <Example heading="Patternfly 3 Badges" handlebars={badgeUpgradeExamplespf3Raw} minHeight="20em">
+        {badgeUpgradeExamplespf3}
+      </Example>
+      <Example
+        heading="Patternfly 4 Badges"
+        handlebars={badgeUpgradeExamplespf4Raw}
+        docs={badgeUpgradeExamplesComplexExampleDoc}
+      >
+        {badgeUpgradeExamplespf4}
+      </Example>
     </Documentation>
   );
 };
