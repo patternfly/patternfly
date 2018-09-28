b7d0a1bac16aa7e88cc4b18a316c806285f078cc
Michael Coker <35148959+michael-coker@users.noreply.github.com>
2018-09-28 14:02:42 -0500

    refactor tooltip and popover components (#747)
    * refactor(popover): refactor popover handlebars and CSS
    
    * popover docs typo
    
    * refactor(tooltip): refactor tooltip handlebars and CSS
    
    * docs and a11y updates
    
    * refactor(vars): update minWidth and minHeight target area var to 44px to match AAA a11y
    
    * rework popover arrow positioning
    
    * add ids, update docs
    
    * refactor transform vars
    
    * typo in popover-arrow.hbs
    
    * update accessibility guide target size
    
    * add **required** where necessary, remove unnecessary popover css
    
    * revert popover a11y docs, change aria-popover to aria-modal, change description of popover modifier classes, add aria-labelledby to tooltip a11y docs, be more descriptive with "always use with modifier class" in tooltip and popover docs, move role and aria-modal attrs from popover-content to the popover component
    
    * whitespace

--- src/patternfly/components/Popover/examples/index.js
+++ src/patternfly/components/Popover/examples/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 
-import PopoverExampleRaw from '!raw!./popover-example.hbs';
+import PopoverTopExampleRaw from '!raw!./popover-top-example.hbs';
+import PopoverRightExampleRaw from '!raw!./popover-right-example.hbs';
+import PopoverBottomExampleRaw from '!raw!./popover-bottom-example.hbs';
+import PopoverLeftExampleRaw from '!raw!./popover-left-example.hbs';
 
-import PopoverExample from './popover-example.hbs';
+import PopoverTopExample from './popover-top-example.hbs';
+import PopoverRightExample from './popover-right-example.hbs';
+import PopoverBottomExample from './popover-bottom-example.hbs';
+import PopoverLeftExample from './popover-left-example.hbs';
 
 import docs from '../docs/code.md';
 import '../styles.scss';
@@ -12,13 +18,25 @@ import '../styles.scss';
 export const Docs = docs;
 
 export default () => {
-  const popoverExample = PopoverExample();
+  const popoverTopExample = PopoverTopExample();
+  const popoverRightExample = PopoverRightExample();
+  const popoverBottomExample = PopoverBottomExample();
+  const popoverLeftExample = PopoverLeftExample();
   const headingText = 'Popover';
 
   return (
     <Documentation docs={Docs} heading={headingText}>
-      <Example className="is-popover" heading="Popover" handlebars={PopoverExampleRaw}>
-        {popoverExample}
+      <Example heading="Popover Top" handlebars={PopoverTopExampleRaw}>
+        {popoverTopExample}
+      </Example>
+      <Example heading="Popover Right" handlebars={PopoverRightExampleRaw}>
+        {popoverRightExample}
+      </Example>
+      <Example heading="Popover Bottom" handlebars={PopoverBottomExampleRaw}>
+        {popoverBottomExample}
+      </Example>
+      <Example heading="Popover Left" handlebars={PopoverLeftExampleRaw}>
+        {popoverLeftExample}
       </Example>
     </Documentation>
   );
