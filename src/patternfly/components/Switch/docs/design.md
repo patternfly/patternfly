d34724cc79f0ff88889f49255735abd119be7c4f
Guillaume Vincent <guillaume@oslab.fr>
2018-10-09 19:37:11 +0200

    Add switch component (#724)

---
+++ src/patternfly/components/Switch/docs/design.md
@@ -0,0 +1,17 @@
+# Component Name
+Switch trigger instant actions a user can take
+
+## Usage
+Use an On/Off Switch when a user is presented with the binary choice to enable or disable some optional attribute or capability. In such a case, Switches are an excellent alternative to a single Checkbox or set of two Radio Buttons.
+
+Since the Switch pattern is only used to enable or disable an attribute, you can simplify labeling when using a Switch by using only the name of the affected attribute. If the options you’re presenting to the user are anything other than “on” or “off,” you should use Radio Buttons instead of Switches.
+
+Just like Radio Buttons and Checkboxes, Switches can be used to enable or disable a set of nested controls.
+
+## Content
+* Switch input aria label should tell the user what will happen with they click the switch.
+* Use specific labels that clearly describe the resource action.
+* Aim for short labels wherever possible.
+* Consistently order labels throughout your application.
+
+See the [PatternFly Terminology and Wording Guidelines](http://www.patternfly.org/styles/terminology-and-wording/) for additional guidance.
---
+++ src/patternfly/components/Switch/examples/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Documentation from '@siteComponents/Documentation';
+import Example from '@siteComponents/Example';
+import SwitchExampleRaw from '!raw!./switch-example.hbs';
+import SwitchDisabledExampleRaw from '!raw!./switch-disabled-example.hbs';
+import SwitchNoLabelExampleRaw from '!raw!./switch-no-label-example.hbs';
+import SwitchExample from './switch-example.hbs';
+import SwitchDisabledExample from './switch-disabled-example.hbs';
+import SwitchNoLabelExample from './switch-no-label-example.hbs';
+import docs from '../docs/code.md';
+import '../styles.scss';
+
+export const Docs = docs;
+
+export default () => {
+  const switchExample = SwitchExample();
+  const switchDisabledExample = SwitchDisabledExample();
+  const switchNoLabelExample = SwitchNoLabelExample();
+
+  return (
+    <Documentation docs={Docs}>
+      <Example heading="Switch" handlebars={SwitchExampleRaw}>
+        {switchExample}
+      </Example>
+      <Example heading="Switch No Label" handlebars={SwitchNoLabelExampleRaw}>
+        {switchNoLabelExample}
+      </Example>
+      <Example heading="Switch Disabled" handlebars={SwitchDisabledExampleRaw}>
+        {switchDisabledExample}
+      </Example>
+    </Documentation>
+  );
+};
