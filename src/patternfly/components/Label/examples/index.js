8dda6af228f75901b3e04747e5bd6d98f5aade92
Christie Molloy <cmolloy@redhat.com>
2018-10-16 13:28:24 -0400

    deleted table from index

--- src/patternfly/components/Label/examples/index.js
+++ src/patternfly/components/Label/examples/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import labelExampleRaw from '!raw!./label-example.hbs';
-import labelTableExampleRaw from '!raw!./label-table-example.hbs';
 import LabelExample from './label-example.hbs';
-import LabelTableExample from './label-table-example.hbs';
 import labelComplexExampleDoc from '../docs/label-complex.md';
 import docs from '../docs/code.md';
 import '../styles.scss';
@@ -13,19 +11,11 @@ export const Docs = docs;
 
 export default () => {
   const labelExample = LabelExample();
-  const labelTableExample = LabelTableExample();
 
   return (
     <Documentation docs={Docs}>
-      <Example heading="Label Component" handlebars={labelExampleRaw}>
+      <Example heading="Label Component" handlebars={labelExampleRaw} docs={labelComplexExampleDoc}>
         {labelExample}
-      </Example>
-      <Example
-        heading="Label in Default Table/Compact Table"
-        handlebars={labelTableExampleRaw}
-        docs={labelComplexExampleDoc}
-      >
-        {labelTableExample}
       </Example>
     </Documentation>
   );
