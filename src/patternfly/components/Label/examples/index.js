e1d14379970af13d9211242f6332cfbcd633f749
Christie Molloy <cmolloy@redhat.com>
2018-10-18 13:25:19 -0400

    feat(label): add label component (#183)
    * feat(label): add label component (#183)
    
    * started css
    
    * made labels
    
    * style changes
    
    * label is finished
    
    * only want one size label
    
    * updates
    
    * add no-wrap to label
    
    * updates made
    
    * review to the label
    
    * updated documentaiton
    
    * deleted table from index
    
    * review

---
+++ src/patternfly/components/Label/examples/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Documentation from '@siteComponents/Documentation';
+import Example from '@siteComponents/Example';
+import labelExampleRaw from '!raw!./label-example.hbs';
+import LabelExample from './label-example.hbs';
+import labelComplexExampleDoc from '../docs/label-complex.md';
+import docs from '../docs/code.md';
+import '../styles.scss';
+
+export const Docs = docs;
+
+export default () => {
+  const labelExample = LabelExample();
+
+  return (
+    <Documentation docs={Docs}>
+      <Example heading="Label Component" handlebars={labelExampleRaw} docs={labelComplexExampleDoc}>
+        {labelExample}
+      </Example>
+    </Documentation>
+  );
+};
