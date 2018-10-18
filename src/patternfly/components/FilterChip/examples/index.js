876e35bc153007f3dc0e288882e46a05e6a04bf7
Christie Molloy <cmolloy@redhat.com>
2018-10-18 10:56:53 -0400

    add filter chips to the toolbar

--- src/patternfly/components/FilterChip/examples/index.js
+++ src/patternfly/components/FilterChip/examples/index.js
@@ -3,8 +3,10 @@ import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import filterChipSimpleExampleRaw from '!raw!./filter-chip-simple-example.hbs';
 import filterChipComplexExampleRaw from '!raw!./filter-chip-complex-example.hbs';
+import filterChipToolbarExampleRaw from '!raw!./filter-chip-toolbar-example.hbs';
 import FilterchipSimpleExample from './filter-chip-simple-example.hbs';
 import FilterchipComplexExample from './filter-chip-complex-example.hbs';
+import FilterchipToolbarExample from './filter-chip-toolbar-example.hbs';
 import filterChipComplexExampleDoc from '../docs/filter-chip-complex.md';
 import docs from '../docs/code.md';
 import '../filter-chip.scss';
@@ -14,11 +16,12 @@ export const Docs = docs;
 export default () => {
   const filterChipSimpleExample = FilterchipSimpleExample();
   const filterChipComplexExample = FilterchipComplexExample();
+  const filterChipToolbarExample = FilterchipToolbarExample();
   const headingText = 'Filter Chip';
 
   return (
     <Documentation docs={Docs} heading={headingText}>
-      <Example heading="Individual Filter Chips" handlebars={filterChipSimpleExampleRaw} minHeight="20em">
+      <Example heading="Individual Filter Chips" handlebars={filterChipSimpleExampleRaw}>
         {filterChipSimpleExample}
       </Example>
       <Example
@@ -27,6 +30,9 @@ export default () => {
         docs={filterChipComplexExampleDoc}
       >
         {filterChipComplexExample}
+      </Example>
+      <Example heading="Filter Chips in the Toolbar" handlebars={filterChipToolbarExampleRaw}>
+        {filterChipToolbarExample}
       </Example>
     </Documentation>
   );
