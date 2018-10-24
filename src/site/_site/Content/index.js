ff5bfda39aeff84ac5cf36a1c3763d643af02e57
Christie Molloy <cmolloy@redhat.com>
2018-10-24 15:58:49 -0400

    Sequence Component (#797)
    * wizard modular structure laid out
    
    * styling for the wizard sequence
    
    * styles to the wizard
    
    * wizard styles
    
    * updating wizard sequence styles
    
    * new styles
    
    * added in link instead of item
    
    * styles
    
    * styles
    
    * styles
    
    * changes to styles
    
    * style updates and complex wizard
    
    * added control
    
    * wizard sequence ready for review
    
    * half through changing from wizard to sequence
    
    * matts review changes
    
    * finish number padding
    
    * a11y
    
    * added active state
    
    * added a11y
    
    * added container for vertical centering
    
    * trying to position toggle buttons on top
    
    * aria
    
    * made changes to a11y
    
    * move control under number so that control takes the top position
    
    * remove status for this example
    
    * hide scrollbar styles
    
    * changes
    
    * moved to global
    
    * removed files from handlebars PR
    
    * file changed
    
    * file same as whats on git
    
    * change file back
    
    * changed file back round 2
    
    * file changed back
    
    * add styles to the global sheet
    
    * numbers now enlarge using transform and scale
    
    * updates
    
    * removed type as it is deprecated
    
    * review changes
    
    * reverse labels
    
    * changes
    
    * changes from review
    
    * changes so far
    
    * updates so far
    
    * changes made from review
    
    * updates
    
    * updates
    
    * change colors to work for light and dark themes
    
    * changes made from Michaels review
    
    * reviewed more items
    
    * added decsription to docs and style changes
    
    * rearrange names by state not property
    
    * removed padding and unnecessary variables
    
    * added media query
    
    * reversed size that buttons appear
    
    * scroll now works on firefox
    
    * turn off flex on the number fixes problem in firefox
    
    * updates
    
    * change sequence number to pseudo element using css counter
    
    * changed variable names to reflect numbers that now use list style
    
    * matts review
    
    * button styles in mozilla
    
    * increased max widthto allow for longer text strings and removed active state because doesnt work in firefox
    
    * change styles
    
    * michaels review
    
    * updates to whitespace, move button styles
    
    * change hbs to structure better in the code preview
    
    * remove arrows disappear at larger breakpoint size. Arrows should appear based on the count of items in the sequence

--- src/site/_site/Content/index.js
+++ src/site/_site/Content/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-export default ({ content, className }) => (
-  <div className={className}>{content}</div>
-);
+export default ({ content, className }) => <div className={className}>{content}</div>;
 
 export const HTMLContent = ({ content, className }) => (
   <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
