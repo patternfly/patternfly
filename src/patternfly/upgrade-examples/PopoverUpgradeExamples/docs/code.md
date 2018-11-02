be17602ef79a7b97f2ac33cde6a0629095614597
James Lau <jalau@redhat.com>
2018-11-02 12:15:52 -0400

    feat(PopoverUpgradeExamples): filling in the folders.

---
+++ src/patternfly/upgrade-examples/PopoverUpgradeExamples/docs/code.md
@@ -0,0 +1,12 @@
+## Overview
+
+When converting PatternFly 3 components to PatternFly 4 components, you must also take into consideration the layouts and sizings that PatternFly 3 utilized from Bootstrap 3.
+
+## Usage
+
+| PF3 Class | Applied To | PF4 Class | Applied To | Outcome |
+| -- | -- | -- | -- | -- |
+| `.popover-pf` | `<div>` | `.pf-c-popover` | `<div>` | Creates a popover containing content. |
+| `.popover-pf-heading` | `<div>` | `.pf-c-popover__header` | `.pf-c-popover` | Creates the header of a popover. |    
+| `.popover-pf-body` | `<div>` | `.pf-c-popover__body` | `.pf-c-popover` | Creates the body of a popover. Required. | 
+| `.popover-pf-footer` | `<div>` | `.pf-c-popover__footer` | `.pf-c-popover` | Creates the footer of a popover. | 
---
+++ src/patternfly/upgrade-examples/PopoverUpgradeExamples/examples/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Documentation from '@siteComponents/Documentation';
+import Example from '@siteComponents/Example';
+import docs from '../docs/code.md';
+import PopoverupgradeexamplesExample1 from './popover-upgrade-examples-example1.hbs';
+import PopoverupgradeexamplesExample2 from './popover-upgrade-examples-example2.hbs';
+
+export const Docs = docs;
+
+export default () => {
+  const PopoverUpgradeExamplesExample1 = PopoverupgradeexamplesExample1();
+  const PopoverUpgradeExamplesExample2 = PopoverupgradeexamplesExample2();
+  const headingText = 'Card Migration';
+
+  return (
+    <Documentation docs={Docs} heading={headingText}>
+      <Example heading="PatternFly 3 Popover">{PopoverUpgradeExamplesExample1}</Example>
+      <Example heading="PatternFly 4 Popover">{PopoverUpgradeExamplesExample2}</Example>
+    </Documentation>
+  );
+};
