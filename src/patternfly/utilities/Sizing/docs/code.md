cc85065c7aa38e7933c0c04dfffe75fba895ac43
Andy Braren <andybraren@gmail.com>
2018-11-05 09:50:27 -0500

    feat(width): Introduce width utility (#820)
    * feat(width): Introduce width utility
    
    This adds a new width utility that enables breakpoint-specific width adjustments.
    
    * Renamed and expanded to become a sizing utility
    
    Adds height and percentage utilities as well.
    
    * Fixed sizing.hbs indentation
    
    * Added border override for button example
    
    * Updated documentation structure
    
    * Add zero width and height utilities
    
    * remove grid helper classes, add workspace class for tall preview body
    
    * Remove breakpoint and button demos
    
    * Added zero width/height previews

---
+++ src/patternfly/utilities/Sizing/docs/code.md
@@ -0,0 +1,3 @@
+## Usage note
+
+These utilities are not recommended for general layout purposes. They should primarily be used to enable responsive behaviors of certain components through breakpoints.
---
+++ src/patternfly/utilities/Sizing/examples/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Documentation from '@siteComponents/Documentation';
+import Example from '@siteComponents/Example';
+
+// Raw
+import sizingExampleWidthRaw from '!raw!./sizing-width.hbs';
+import sizingExampleHeightRaw from '!raw!./sizing-height.hbs';
+
+// Width
+import SizingExampleWidth from './sizing-width.hbs';
+import sizingWidthDocs from '../docs/sizing-width.md';
+
+// Height
+import SizingExampleHeight from './sizing-height.hbs';
+import sizingHeightDocs from '../docs/sizing-height.md';
+
+import docs from '../docs/code.md';
+import '../sizing.scss';
+
+export const Docs = docs;
+export const headingText = 'Sizing';
+
+export default () => {
+  const sizingExampleWidth = SizingExampleWidth();
+  const sizingExampleHeight = SizingExampleHeight();
+
+  return (
+    <Documentation docs={Docs} heading={headingText} className="is-utility-page">
+      <Example heading="Width" handlebars={sizingExampleWidthRaw} docs={sizingWidthDocs}>
+        {sizingExampleWidth}
+      </Example>
+      <Example heading="Height" handlebars={sizingExampleHeightRaw} docs={sizingHeightDocs} className="is-tall-body">
+        {sizingExampleHeight}
+      </Example>
+    </Documentation>
+  );
+};
