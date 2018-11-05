cdeef447e53af0ef09c90c83c582143dd79f91c6
andybraren <andybraren@gmail.com>
2018-11-05 09:19:28 -0500

    Remove breakpoint and button demos

--- src/patternfly/utilities/Sizing/examples/index.js
+++ src/patternfly/utilities/Sizing/examples/index.js
@@ -5,8 +5,6 @@ import Example from '@siteComponents/Example';
 // Raw
 import sizingExampleWidthRaw from '!raw!./sizing-width.hbs';
 import sizingExampleHeightRaw from '!raw!./sizing-height.hbs';
-import sizingExampleBreakpointRaw from '!raw!./sizing-breakpoint.hbs';
-import sizingExampleButtonRaw from '!raw!./sizing-button.hbs';
 
 // Width
 import SizingExampleWidth from './sizing-width.hbs';
@@ -15,12 +13,6 @@ import sizingWidthDocs from '../docs/sizing-width.md';
 // Height
 import SizingExampleHeight from './sizing-height.hbs';
 import sizingHeightDocs from '../docs/sizing-height.md';
-
-// Breakpoint demo
-import SizingExampleBreakpoint from './sizing-breakpoint.hbs';
-
-// Button demo
-import SizingExampleButton from './sizing-button.hbs';
 
 import docs from '../docs/code.md';
 import '../sizing.scss';
@@ -31,8 +23,6 @@ export const headingText = 'Sizing';
 export default () => {
   const sizingExampleWidth = SizingExampleWidth();
   const sizingExampleHeight = SizingExampleHeight();
-  const sizingExampleBreakpoint = SizingExampleBreakpoint();
-  const sizingExampleButton = SizingExampleButton();
 
   return (
     <Documentation docs={Docs} heading={headingText} className="is-utility-page">
@@ -41,12 +31,6 @@ export default () => {
       </Example>
       <Example heading="Height" handlebars={sizingExampleHeightRaw} docs={sizingHeightDocs} className="is-tall-body">
         {sizingExampleHeight}
-      </Example>
-      <Example heading="Breakpoint demo" handlebars={sizingExampleBreakpointRaw}>
-        {sizingExampleBreakpoint}
-      </Example>
-      <Example heading="Button demo" handlebars={sizingExampleButtonRaw}>
-        {sizingExampleButton}
       </Example>
     </Documentation>
   );
