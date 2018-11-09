777ef019847090a5ab12aa7e0e74a8762820060c
Christie Molloy <cmolloy@redhat.com>
2018-11-09 09:43:42 -0600

    changes

--- src/patternfly/components/ChipGroup/examples/index.js
+++ src/patternfly/components/ChipGroup/examples/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
-import ChipGroupExampleRaw from '!raw!./chip-group-example.hbs';
-import chipGroupExample from './chip-group-example.hbs';
+import ChipGroupToolbarExampleRaw from '!raw!./chip-group-toolbar-example.hbs';
+import ChipGroupSelectExampleRaw from '!raw!./chip-group-select-example.hbs';
+import chipGroupToolbarExample from './chip-group-toolbar-example.hbs';
+import chipGroupSelectExample from './chip-group-select-example.hbs';
 import ChipGroupExampleDoc from '../docs/chip-group.md';
 import docs from '../docs/code.md';
 import '../chip-group.scss';
@@ -10,13 +12,21 @@ import '../chip-group.scss';
 export const Docs = docs;
 
 export default () => {
-  const ChipGroupExample = chipGroupExample();
+  const ChipGroupToolbarExample = chipGroupToolbarExample();
+  const ChipGroupSelectExample = chipGroupSelectExample();
   const headingText = 'Chip Group';
 
   return (
     <Documentation docs={Docs} heading={headingText}>
-      <Example heading="Grouped Chips" handlebars={ChipGroupExampleRaw} docs={ChipGroupExampleDoc}>
-        {ChipGroupExample}
+      <Example heading="Grouped Chips in Toolbar" handlebars={ChipGroupToolbarExampleRaw}>
+        {ChipGroupToolbarExample}
+      </Example>
+      <Example
+        heading="Grouped Chips in Multi-Select"
+        handlebars={ChipGroupSelectExampleRaw}
+        docs={ChipGroupExampleDoc}
+      >
+        {ChipGroupSelectExample}
       </Example>
     </Documentation>
   );
