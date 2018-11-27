e7086d2adc60ba28ed5c0c525f54e763d40af874
Michael Coker <35148959+mcoker@users.noreply.github.com>
2018-11-27 15:39:24 -0600

    feat(login): create login component from login demo and login-box component (#964)
    * feat(login): create login component from login demo and login-box component
    
    * fix space between login button and checkbox
    
    * remove pf-l-login from workspace.scss, change pf-l-login to pf-c-login in login docs
    
    * update mq var names, remove flex on main section
    
    * change main area header/footer divs to header/footer elements
    
    * remove loginpage demo

--- src/patternfly/components/Login/examples/index.js
+++ src/patternfly/components/Login/examples/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
-import loginSimpleExampleRaw from '!raw!./login-example.hbs';
-import LoginSimpleExample from './login-example.hbs';
+import loginSimpleExampleRaw from '!raw!./login-simple-example.hbs';
+import LoginSimpleExample from './login-simple-example.hbs';
 import docs from '../docs/code.md';
 import '../login.scss';
 
@@ -10,11 +10,10 @@ export const Docs = docs;
 
 export default () => {
   const loginSimpleExample = LoginSimpleExample();
-  const headingText = 'Login';
 
   return (
-    <Documentation docs={Docs} heading={headingText} className="is-layout-page">
-      <Example heading="Login Simple" fullPageOnly="true" handlebars={loginSimpleExampleRaw} minHeight="20em">
+    <Documentation docs={Docs}>
+      <Example heading="Login" fullPageOnly="true" handlebars={loginSimpleExampleRaw}>
         {loginSimpleExample}
       </Example>
     </Documentation>
