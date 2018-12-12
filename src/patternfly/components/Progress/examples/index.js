c827af49c35d3da70b9bc1b4a2a6beec353759c5
Christie Molloy <cmolloy@redhat.com>
2018-12-12 15:21:41 -0500

    refactor(progress): refactor css vars

--- src/patternfly/components/Progress/examples/index.js
+++ src/patternfly/components/Progress/examples/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import progressSimpleExampleRaw from '!raw!./progress-simple-example.hbs';
+import progressNoDescriptionExampleRaw from '!raw!./progress-no-description-example.hbs';
 import progressSmallExampleRaw from '!raw!./progress-small-example.hbs';
 import progressLargeExampleRaw from '!raw!./progress-large-example.hbs';
 import progressOutsideExampleRaw from '!raw!./progress-outside-example.hbs';
@@ -14,6 +15,7 @@ import progressNoMeasureExampleRaw from '!raw!./progress-no-measure-example.hbs'
 import progressNoMeasureFailureExampleRaw from '!raw!./progress-no-measure-failure-example.hbs';
 import progressDynamicExampleRaw from '!raw!./progress-dynamic-example.hbs';
 import ProgressSimpleExample from './progress-simple-example.hbs';
+import ProgressNoDescriptionExample from './progress-no-description-example.hbs';
 import ProgressSmallExample from './progress-small-example.hbs';
 import ProgressLargeExample from './progress-large-example.hbs';
 import ProgressOutsideExample from './progress-outside-example.hbs';
@@ -33,6 +35,7 @@ export const Docs = docs;
 
 export default () => {
   const progressSimpleExample = ProgressSimpleExample();
+  const progressNoDescriptionExample = ProgressNoDescriptionExample();
   const progressSmallExample = ProgressSmallExample();
   const progressLargeExample = ProgressLargeExample();
   const progressOutsideExample = ProgressOutsideExample();
@@ -74,6 +77,9 @@ export default () => {
       </Example>
       <Example heading="Progress Outside Failure" handlebars={progressOutsideFailureExampleRaw}>
         {progressOutsideFailureExample}
+      </Example>
+      <Example heading="Progress Without Description" handlebars={progressNoDescriptionExampleRaw}>
+        {progressNoDescriptionExample}
       </Example>
       <Example heading="Progress Without Measure" handlebars={progressNoMeasureExampleRaw}>
         {progressNoMeasureExample}
