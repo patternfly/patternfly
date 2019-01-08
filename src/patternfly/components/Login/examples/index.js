4bc452a1e2b12ef87023e6dc421f16efef323882
Michael Coker <35148959+mcoker@users.noreply.github.com>
2019-01-08 13:10:32 -0600

    feature(login): add error state to login page component (#1045)

--- src/patternfly/components/Login/examples/index.js
+++ src/patternfly/components/Login/examples/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import loginSimpleExampleRaw from '!raw!./login-simple-example.hbs';
+import loginInvalidExampleRaw from '!raw!./login-invalid-example.hbs';
 import LoginSimpleExample from './login-simple-example.hbs';
+import LoginInvalidExample from './login-invalid-example.hbs';
 import docs from '../docs/code.md';
 import '../login.scss';
 
@@ -10,11 +12,15 @@ export const Docs = docs;
 
 export default () => {
   const loginSimpleExample = LoginSimpleExample();
+  const loginInvalidExample = LoginInvalidExample();
 
   return (
     <Documentation docs={Docs}>
-      <Example heading="Login" fullPageOnly="true" handlebars={loginSimpleExampleRaw}>
+      <Example heading="Login Simple" fullPageOnly="true" handlebars={loginSimpleExampleRaw}>
         {loginSimpleExample}
+      </Example>
+      <Example heading="Login Invalid" fullPageOnly="true" handlebars={loginInvalidExampleRaw}>
+        {loginInvalidExample}
       </Example>
     </Documentation>
   );
