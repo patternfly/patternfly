7e15710a105ddf7bd34a469cbae385386bf2be92
Michael Coker <35148959+mcoker@users.noreply.github.com>
2019-01-15 11:16:35 -0600

    chore(workspace): remove FA scripts from layout pages, update FA resources, fix conflicts with pf shield (#1221)
    * chore(workspace): remove FA scripts from layout pages, update FA resources, fix conflicts with pf shield
    
    * rebase and update layout pages to remove FA scripts

--- src/site/_site/FullPageExampleLayout/index.js
+++ src/site/_site/FullPageExampleLayout/index.js
@@ -7,18 +7,6 @@ export default ({ children }) => (
     <Helmet>
       <title>PF Next</title>
       <html lang="en-US" />
-      <script
-        defer
-        src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js"
-        integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ"
-        crossOrigin="anonymous"
-      />
-      <script
-        defer
-        src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js"
-        integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY"
-        crossOrigin="anonymous"
-      />
     </Helmet>
     <div>{children}</div>
   </div>
