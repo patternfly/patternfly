9292231bf90516e749eabaab36c02a5f4b600254
Matthew Carleton <mcarleton@redhat.com>
2019-01-24 16:54:21 -0400

    fix(chip): add badge

--- src/patternfly/components/Chip/examples/index.js
+++ src/patternfly/components/Chip/examples/index.js
@@ -3,8 +3,6 @@ import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import ChipExampleRaw from '!raw!./chip-example.hbs';
 import chipExample from './chip-example.hbs';
-
-import ChipExampleDoc from '../docs/chip.md';
 import docs from '../docs/code.md';
 import '../chip.scss';
 
@@ -15,8 +13,8 @@ export default () => {
   const headingText = 'Chip';
 
   return (
-    <Documentation heading={headingText}>
-      <Example heading="Chip" handlebars={ChipExampleRaw} className="is-light-preview" docs={ChipExampleDoc}>
+    <Documentation docs={Docs} heading={headingText}>
+      <Example heading="Chip" handlebars={ChipExampleRaw} className="is-light-preview">
         {ChipExample}
       </Example>
     </Documentation>
