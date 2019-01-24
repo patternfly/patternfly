b7be5eefd920307d55638d5e334157072c78b765
Michael Coker <mcoker@redhat.com>
2019-01-24 10:53:25 -0600

    update docs

---
+++ src/patternfly/components/Pagination/docs/pagination-bottom-example.md
@@ -0,0 +1,1 @@
+Unlike the top pagination, the bottom pagination does not change in a responsive view and the Options Menu persists on all viewports.
--- src/patternfly/components/Pagination/examples/index.js
+++ src/patternfly/components/Pagination/examples/index.js
@@ -7,7 +7,8 @@ import paginationBottomExampleRaw from '!raw!./pagination-bottom-example.hbs';
 import PaginationTopExample from './pagination-top-example.hbs';
 import PaginationTopExpandedExample from './pagination-top-expanded-example.hbs';
 import PaginationBottomExample from './pagination-bottom-example.hbs';
-import paginationExampleDoc from '../docs/pagination-example.md';
+import paginationTopExampleDoc from '../docs/pagination-top-example.md';
+import paginationBottomExampleDoc from '../docs/pagination-bottom-example.md';
 import docs from '../docs/code.md';
 import '../pagination.scss';
 
@@ -19,14 +20,24 @@ export default () => {
   const paginationBottomExample = PaginationBottomExample();
 
   return (
-    <Documentation docs={paginationExampleDoc}>
-      <Example heading="Pagination Top" handlebars={paginationTopExampleRaw} fullPageOnly="true">
+    <Documentation docs={docs}>
+      <Example
+        heading="Pagination Top"
+        handlebars={paginationTopExampleRaw}
+        fullPageOnly="true"
+        docs={paginationTopExampleDoc}
+      >
         {paginationTopExample}
       </Example>
       <Example heading="Pagination Top Expanded" handlebars={paginationTopExpandedExampleRaw} fullPageOnly="true">
         {paginationTopExpandedExample}
       </Example>
-      <Example heading="Pagination Bottom" handlebars={paginationBottomExampleRaw} fullPageOnly="true">
+      <Example
+        heading="Pagination Bottom"
+        handlebars={paginationBottomExampleRaw}
+        fullPageOnly="true"
+        docs={paginationBottomExampleDoc}
+      >
         {paginationBottomExample}
       </Example>
     </Documentation>
