081edaad40ac42c55a57f25f3299da3bdebcd624
Michael Coker <35148959+mcoker@users.noreply.github.com>
2019-01-24 09:04:27 -0600

    feat(radio): refactor check component to remove radio and create radio component (#1292)
    * feat(radio): refactor check component to remove radio and create radio component
    
    * update name in check examples
    
    * update example

--- src/patternfly/components/Radio/examples/index.js
+++ src/patternfly/components/Radio/examples/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
-import CheckExampleRaw from '!raw!./check-example.hbs';
-import CheckExample from './check-example.hbs';
+import RadioExampleRaw from '!raw!./radio-example.hbs';
+import RadioExample from './radio-example.hbs';
 import docs from '../docs/code.md';
-import '../check.scss';
+import '../radio.scss';
 
 export const Docs = docs;
 
 export default () => {
-  const checkExample = CheckExample();
-  const headingText = 'Check';
+  const radioExample = RadioExample();
+  const headingText = 'Radio';
 
   return (
     <Documentation docs={Docs} heading={headingText}>
-      <Example heading="Check Example" handlebars={CheckExampleRaw}>
-        {checkExample}
+      <Example heading="Radio Example" handlebars={RadioExampleRaw}>
+        {radioExample}
       </Example>
     </Documentation>
   );
