086836f0782e78a1e9ba05315be0705e2394cfba
Christie Molloy <cmolloy@redhat.com>
2019-01-30 11:29:13 -0500

    updates

--- src/patternfly/components/ClipboardCopy/docs/code.md
+++ src/patternfly/components/ClipboardCopy/docs/code.md
@@ -4,4 +4,9 @@
 
 | Class | Applied To | Outcome |
 | -- | -- | -- |
-
+| `.pf-c-clipboard-copy` | `<div>` | Initiates a clipboard copy component. |
+| `.pf-c-clipboard-copy__group` | `<div>` | Initiates a wrapper for the clipboard copy group. |
+| `.pf-c-clipboard-copy__group-toggle` | `<button>` | Initiates a toggle button that expands content. |
+| `.pf-c-clipboard-copy__group-text` | `<span>` |  Initiates text within the clipboard copy component. **Required** |
+| `.pf-c-clipboard-copy__group-copy` | `<button>` | Initiates a copy button with the clipboard icon. **Required** |
+| `.pf-c-clipboard-copy__expanded-content` | `<div>` | Initiates an expanded text element. |
--- src/patternfly/components/ClipboardCopy/examples/index.js
+++ src/patternfly/components/ClipboardCopy/examples/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
-import ClipboardCopyExampleRaw from '!raw!./clipboard-copy-example.hbs';
-import ClipboardCopyExample from './clipboard-copy-example.hbs';
+import ClipboardCopyNoExpansionExampleRaw from '!raw!./clipboard-copy-no-expansion-example.hbs';
+import ClipboardCopyExpansionExampleRaw from '!raw!./clipboard-copy-expansion-example.hbs';
+import ClipboardCopyNoExpansionExample from './clipboard-copy-no-expansion-example.hbs';
+import ClipboardCopyExpansionExample from './clipboard-copy-expansion-example.hbs';
 import docs from '../docs/code.md';
 import '../clipboard-copy.scss';
 
 export const Docs = docs;
 
 export default () => {
-  const clipboardCopyExample = ClipboardCopyExample();
+  const clipboardCopyNoExpansionExample = ClipboardCopyNoExpansionExample();
+  const clipboardCopyExpansionExample = ClipboardCopyExpansionExample();
   const headingText = 'Copy to Clipboard';
 
   return (
     <Documentation docs={Docs} heading={headingText}>
-      <Example heading="Copy to Clipboard Examples" handlebars={ClipboardCopyExampleRaw} minHeight="20em">
-        {clipboardCopyExample}
+      <Example heading="Copy to Clipboard No Expansion" handlebars={ClipboardCopyNoExpansionExampleRaw}>
+        {clipboardCopyNoExpansionExample}
+      </Example>
+      <Example heading="Copy to Clipboard Expansion" handlebars={ClipboardCopyExpansionExampleRaw} minHeight="20em">
+        {clipboardCopyExpansionExample}
       </Example>
     </Documentation>
   );
