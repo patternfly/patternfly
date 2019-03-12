3649961fcf04dd47def72db50a2e683c00dd24b8
Michael Coker <mcoker@redhat.com>
2019-03-12 17:37:50 -0500

    fix(table): compact table expanded state styling

---
+++ src/patternfly/components/Table/docs/table-compact-expandable.md
@@ -0,0 +1,5 @@
+### Table Expandable Notes
+
+<mark>**All compact table accessibility and usage requirements apply.**</mark>
+<br>
+<mark>**All expandable table accessibility and usage requirements apply.**</mark>
--- src/patternfly/components/Table/examples/index.js
+++ src/patternfly/components/Table/examples/index.js
@@ -6,7 +6,11 @@ import tableSortableExampleRaw from '!raw!./table-sortable-example.hbs';
 import tableSimpleWithCheckboxesExampleRaw from '!raw!./table-simple-with-checkboxes-example.hbs';
 import tableExpandableExampleRaw from '!raw!./table-expandable-example.hbs';
 import tableCompactExampleRaw from '!raw!./table-compact-example.hbs';
+<<<<<<< HEAD
 import tableCompactBorderRowsExampleRaw from '!raw!./table-compact-border-rows-example.hbs';
+=======
+import tableCompactExpandableExampleRaw from '!raw!./table-compact-expandable-example.hbs';
+>>>>>>> fix(table): compact table expanded state styling
 import tableWidthExampleRaw from '!raw!./table-width-example.hbs';
 import tableCompoundExpansionExampleRaw from '!raw!./table-compound-expansion-example.hbs';
 
@@ -25,8 +29,13 @@ import tableExpandableDoc from '../docs/table-expandable.md';
 import TableCompactExample from './table-compact-example.hbs';
 import tableCompactDoc from '../docs/table-compact.md';
 
+<<<<<<< HEAD
 import TableCompactBorderRowsExample from './table-compact-border-rows-example.hbs';
 import tableCompactBorderRowsDoc from '../docs/table-compact-border-rows.md';
+=======
+import TableCompactExpandableExample from './table-compact-expandable-example.hbs';
+import tableCompactExpandableDoc from '../docs/table-compact-expandable.md';
+>>>>>>> fix(table): compact table expanded state styling
 
 import TableWidthExample from './table-width-example.hbs';
 import tableWidthDoc from '../docs/table-width.md';
@@ -43,7 +52,11 @@ export default () => {
   const tableSortableExample = TableSortableExample();
   const tableExpandableExample = TableExpandableExample();
   const tableCompactExample = TableCompactExample();
+<<<<<<< HEAD
   const tableCompactBorderRowsExample = TableCompactBorderRowsExample();
+=======
+  const tableCompactExpandableExample = TableCompactExpandableExample();
+>>>>>>> fix(table): compact table expanded state styling
   const tableSimpleWithCheckboxesExample = TableSimpleWithCheckboxesExample();
   const tableWidthExample = TableWidthExample();
   const tableCompoundExpansionExample = TableCompoundExpansionExample();
@@ -79,11 +92,19 @@ export default () => {
         {tableCompactExample}
       </Example>
       <Example
+<<<<<<< HEAD
         heading="Compact Table with borders"
         handlebars={tableCompactBorderRowsExampleRaw}
         docs={tableCompactBorderRowsDoc}
       >
         {tableCompactBorderRowsExample}
+=======
+        heading="Compact Expandable Table"
+        handlebars={tableCompactExpandableExampleRaw}
+        docs={tableCompactExpandableDoc}
+      >
+        {tableCompactExpandableExample}
+>>>>>>> fix(table): compact table expanded state styling
       </Example>
       <Example heading="Table with Width Modifiers" handlebars={tableWidthExampleRaw} docs={tableWidthDoc}>
         {tableWidthExample}
