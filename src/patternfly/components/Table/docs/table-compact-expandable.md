71876460252cfbd9af87007dad96d44cc05b4ebc
Michael Coker <mcoker@redhat.com>
2019-03-15 12:57:32 -0500

    refactor(table): add borders to compact table, add no border modifier

--- src/patternfly/components/Table/docs/table-compact-expandable.md
+++ src/patternfly/components/Table/docs/table-compact-expandable.md
@@ -3,3 +3,8 @@
 <mark>**All compact table accessibility and usage requirements apply.**</mark>
 <br>
 <mark>**All expandable table accessibility and usage requirements apply.**</mark>
+
+
+| Class | Applied To | Outcome |
+| -- | -- | -- |
+| `.pf-m-expandable` | `.pf-c-table.pf-m-compact` | Indicates that the table has expandable rows. **Note: Can not be used with `.pf-m-no-border-rows`.** |
--- src/patternfly/components/Table/examples/index.js
+++ src/patternfly/components/Table/examples/index.js
@@ -6,11 +6,8 @@ import tableSortableExampleRaw from '!raw!./table-sortable-example.hbs';
 import tableSimpleWithCheckboxesExampleRaw from '!raw!./table-simple-with-checkboxes-example.hbs';
 import tableExpandableExampleRaw from '!raw!./table-expandable-example.hbs';
 import tableCompactExampleRaw from '!raw!./table-compact-example.hbs';
-<<<<<<< HEAD
-import tableCompactBorderRowsExampleRaw from '!raw!./table-compact-border-rows-example.hbs';
-=======
+import tableCompactNoBorderRowsExampleRaw from '!raw!./table-compact-no-border-rows-example.hbs';
 import tableCompactExpandableExampleRaw from '!raw!./table-compact-expandable-example.hbs';
->>>>>>> fix(table): compact table expanded state styling
 import tableWidthExampleRaw from '!raw!./table-width-example.hbs';
 import tableCompoundExpansionExampleRaw from '!raw!./table-compound-expansion-example.hbs';
 
@@ -29,13 +26,10 @@ import tableExpandableDoc from '../docs/table-expandable.md';
 import TableCompactExample from './table-compact-example.hbs';
 import tableCompactDoc from '../docs/table-compact.md';
 
-<<<<<<< HEAD
-import TableCompactBorderRowsExample from './table-compact-border-rows-example.hbs';
-import tableCompactBorderRowsDoc from '../docs/table-compact-border-rows.md';
-=======
+import TableCompactNoBorderRowsExample from './table-compact-no-border-rows-example.hbs';
+import tableCompactNoBorderRowsDoc from '../docs/table-compact-no-border-rows.md';
 import TableCompactExpandableExample from './table-compact-expandable-example.hbs';
 import tableCompactExpandableDoc from '../docs/table-compact-expandable.md';
->>>>>>> fix(table): compact table expanded state styling
 
 import TableWidthExample from './table-width-example.hbs';
 import tableWidthDoc from '../docs/table-width.md';
@@ -52,11 +46,8 @@ export default () => {
   const tableSortableExample = TableSortableExample();
   const tableExpandableExample = TableExpandableExample();
   const tableCompactExample = TableCompactExample();
-<<<<<<< HEAD
-  const tableCompactBorderRowsExample = TableCompactBorderRowsExample();
-=======
+  const tableCompactNoBorderRowsExample = TableCompactNoBorderRowsExample();
   const tableCompactExpandableExample = TableCompactExpandableExample();
->>>>>>> fix(table): compact table expanded state styling
   const tableSimpleWithCheckboxesExample = TableSimpleWithCheckboxesExample();
   const tableWidthExample = TableWidthExample();
   const tableCompoundExpansionExample = TableCompoundExpansionExample();
@@ -92,19 +83,18 @@ export default () => {
         {tableCompactExample}
       </Example>
       <Example
-<<<<<<< HEAD
-        heading="Compact Table with borders"
-        handlebars={tableCompactBorderRowsExampleRaw}
-        docs={tableCompactBorderRowsDoc}
+        heading="Compact Table with no borders"
+        handlebars={tableCompactNoBorderRowsExampleRaw}
+        docs={tableCompactNoBorderRowsDoc}
       >
-        {tableCompactBorderRowsExample}
-=======
+        {tableCompactNoBorderRowsExample}
+      </Example>
+      <Example
         heading="Compact Expandable Table"
         handlebars={tableCompactExpandableExampleRaw}
         docs={tableCompactExpandableDoc}
       >
         {tableCompactExpandableExample}
->>>>>>> fix(table): compact table expanded state styling
       </Example>
       <Example heading="Table with Width Modifiers" handlebars={tableWidthExampleRaw} docs={tableWidthDoc}>
         {tableWidthExample}
