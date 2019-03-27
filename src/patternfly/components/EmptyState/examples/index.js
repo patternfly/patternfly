599344550650f9f3e529e7bdcf6e9d48aad1013c
Christie Molloy <cmolloy@redhat.com>
2019-03-27 17:15:20 -0400

    fix(emptyState): add sm and lg modifiers for max-width (#1475)

--- src/patternfly/components/EmptyState/examples/index.js
+++ src/patternfly/components/EmptyState/examples/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import emptyStateSimpleExampleRaw from '!raw!./empty-state-simple-example.hbs';
+import emptyStateSmallExampleRaw from '!raw!./empty-state-small-example.hbs';
+import emptyStateLargeExampleRaw from '!raw!./empty-state-large-example.hbs';
 import EmptystateSimpleExample from './empty-state-simple-example.hbs';
+import EmptystateSmallExample from './empty-state-small-example.hbs';
+import EmptystateLargeExample from './empty-state-large-example.hbs';
 import docs from '../docs/code.md';
 
 export const Docs = docs;
 
 export default () => {
   const emptyStateSimpleExample = EmptystateSimpleExample();
+  const emptyStateSmallExample = EmptystateSmallExample();
+  const emptyStateLargeExample = EmptystateLargeExample();
   const headingText = 'Empty State';
   const variablesRoot = 'pf-c-empty-state';
 
@@ -16,6 +22,12 @@ export default () => {
     <Documentation docs={Docs} heading={headingText} variablesRoot={variablesRoot}>
       <Example heading="Empty State" handlebars={emptyStateSimpleExampleRaw}>
         {emptyStateSimpleExample}
+      </Example>
+      <Example heading="Empty State Small" handlebars={emptyStateSmallExampleRaw}>
+        {emptyStateSmallExample}
+      </Example>
+      <Example heading="Empty State Large" handlebars={emptyStateLargeExampleRaw}>
+        {emptyStateLargeExample}
       </Example>
     </Documentation>
   );
