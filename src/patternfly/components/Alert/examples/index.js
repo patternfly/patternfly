04641c90d6858a56aab9695feb58eb242efba83b
Sarah Rambacher <srambach@redhat.com>
2019-05-23 12:52:15 -0400

    feat(alert): adds inline modifier (#1775)
    * feat(alert): adds inline modifier

--- src/patternfly/components/Alert/examples/index.js
+++ src/patternfly/components/Alert/examples/index.js
@@ -4,15 +4,21 @@ import Example from '@siteComponents/Example';
 
 import AlertTypesRaw from '!raw!./alert-types-example.hbs';
 import AlertVariationsRaw from '!raw!./alert-variations-example.hbs';
+import AlertInlineRaw from '!raw!./alert-inline-example.hbs';
+import AlertInlineVariationsRaw from '!raw!./alert-inline-variations-example.hbs';
 import AlertTypes from './alert-types-example.hbs';
 import AlertVariations from './alert-variations-example.hbs';
+import AlertInline from './alert-inline-example.hbs';
+import AlertInlineVariations from './alert-inline-variations-example.hbs';
 import docs from '../docs/code.md';
 
 export const Docs = docs;
 
-export default (props) => {
+export default props => {
   const alertTypes = AlertTypes();
   const alertVariations = AlertVariations();
+  const alertInline = AlertInline();
+  const alertInlineVariations = AlertInlineVariations();
   const headingText = 'Alert';
   const variablesRoot = 'pf-c-alert';
 
@@ -23,6 +29,12 @@ export default (props) => {
       </Example>
       <Example heading="Alert variations" handlebars={AlertVariationsRaw}>
         {alertVariations}
+      </Example>
+      <Example heading="Inline alert types" handlebars={AlertInlineRaw}>
+        {alertInline}
+      </Example>
+      <Example heading="Inline alert variations" handlebars={AlertInlineVariationsRaw}>
+        {alertInlineVariations}
       </Example>
     </Documentation>
   );
