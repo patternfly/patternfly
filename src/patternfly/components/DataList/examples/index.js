65e09063901cf4c72b2c2159f0906244c8de06a0
jenny-s51 <jshandel12@gmail.com>
2019-05-30 15:41:18 -0400

    fix(consistency): removed extra commits

--- src/patternfly/components/DataList/examples/index.js
+++ src/patternfly/components/DataList/examples/index.js
@@ -2,19 +2,16 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import DataListSimpleExampleRaw from '!raw!./data-list-simple-example.hbs';
-import DataListSimpleH2ExampleRaw from '!raw!./data-list-simple-h2-example.hbs';
 import DataListCheckActionExampleRaw from '!raw!./data-list-check-action-example.hbs';
 import DataListExpandableExampleRaw from '!raw!./data-list-expandable-example.hbs';
 import DataListWidthExampleRaw from '!raw!./data-list-width-example.hbs';
 
 import DataListSimpleExample from './data-list-simple-example.hbs';
-import DataListSimpleH2Example from './data-list-simple-h2-example.hbs';
 import DataListCheckActionExample from './data-list-check-action-example.hbs';
 import DataListExpandableExample from './data-list-expandable-example.hbs';
 import DataListWidthExample from './data-list-width-example.hbs';
 
 import DataListSimpleExampleDoc from '../docs/data-list-simple.md';
-import DataListSimpleH2ExampleDoc from '../docs/data-list-simple-h2.md';
 import DataListCheckActionExampleDoc from '../docs/data-list-check-action.md';
 import DataListExpandableExampleDoc from '../docs/data-list-expandable.md';
 import DataListWidthExampleDoc from '../docs/data-list-width.md';
@@ -25,7 +22,6 @@ export const Docs = docs;
 
 export default props => {
   const dataListSimpleExample = DataListSimpleExample();
-  const dataListSimpleH2Example = DataListSimpleH2Example();
   const dataListCheckActionExample = DataListCheckActionExample();
   const dataListExpandableExample = DataListExpandableExample();
   const dataListWidthExample = DataListWidthExample();
@@ -36,13 +32,6 @@ export default props => {
     <Documentation data={props} docs={Docs} heading={headingText} variablesRoot={variablesRoot}>
       <Example heading="Data list simple" handlebars={DataListSimpleExampleRaw} docs={DataListSimpleExampleDoc}>
         {dataListSimpleExample}
-      </Example>
-      <Example
-        heading="Data list simple with headings"
-        handlebars={DataListSimpleH2ExampleRaw}
-        docs={DataListSimpleH2ExampleDoc}
-      >
-        {dataListSimpleH2Example}
       </Example>
       <Example
         heading="Data list checkboxes, actions and additional cells"
