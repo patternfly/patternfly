f01c78bc88e90d849258fc9333e5d811d1bb0ae5
redallen <zallen@redhat.com>
2019-05-31 12:50:19 -0400

    feat(ie11): add ie11 to build

---
+++ build/npm-scripts/ie-convert-all.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fs = require('fs');
+const { getStylesheetPaths, transform } = require('./ie-conversion-utils');
+
+const pfStylesheetsGlob = path.resolve(__dirname, '../../dist/{components,layouts,utilities}/**/*.css');
+const patternflyBasePath = path.resolve(__dirname, '../../dist/patternfly-base.css');
+const outPath = path.resolve(__dirname, '../../dist/patternfly-ie11.css');
+
+getStylesheetPaths(pfStylesheetsGlob, [], [])
+  .then(files =>
+    files.reduce((acc, file) => {
+      acc += `${fs.readFileSync(file, 'utf8')}\n`;
+      return acc;
+    }, '')
+  )
+  .then(concatCss => transform(concatCss, patternflyBasePath))
+  .then(transformedCss => transformedCss.replace(/\.\.\/\.\.\/assets/gm, './assets'))
+  .then(ie11ReadyStylesheet => fs.writeFileSync(outPath, ie11ReadyStylesheet))
+  .catch(error => {
+    throw new Error(error);
+  });
