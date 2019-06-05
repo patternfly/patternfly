f0af9e7851e24b165e4078804ab8bc542a66e52e
Michael Coker <mcoker@redhat.com>
2019-06-05 12:44:00 -0500

    docs(card): update action order in img/action examples, rename demo

---
+++ src/patternfly/demos/CardView/examples/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Documentation from '@siteComponents/Documentation';
+import Example from '@siteComponents/Example';
+
+import CardViewDemoExampleRaw from '!raw!./card-view-demo-example.hbs';
+import CardViewDemoExample from './card-view-demo-example.hbs';
+
+import docs from '../docs/code.md';
+
+export const Docs = docs;
+
+export default () => {
+  const cardViewDemoExample = CardViewDemoExample();
+
+  const headingText = 'Card view demo';
+
+  return (
+    <Documentation docs={Docs} heading={headingText}>
+      <Example heading="Card view demo" fullPageOnly="true" handlebars={CardViewDemoExampleRaw}>
+        {cardViewDemoExample}
+      </Example>
+    </Documentation>
+  );
+};
