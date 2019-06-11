ca2f9fb8fd5be89633b7d0d59a0122dc24785594
Michael Coker <mcoker@redhat.com>
2019-06-11 16:45:07 -0500

    feat(applauncher): add support for sections/icons/divider

--- src/patternfly/components/AppLauncher/examples/index.js
+++ src/patternfly/components/AppLauncher/examples/index.js
@@ -3,15 +3,18 @@ import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import AppLauncherCollapsedExampleRaw from '!raw!./app-launcher-collapsed-example.hbs';
 import AppLauncherExpandedExampleRaw from '!raw!./app-launcher-expanded-example.hbs';
+import AppLauncherSectionsIconsExampleRaw from '!raw!./app-launcher-sections-icons-example.hbs';
 import ApplauncherCollapsedExample from './app-launcher-collapsed-example.hbs';
 import ApplauncherExpandedExample from './app-launcher-expanded-example.hbs';
+import ApplauncherSectionsIconsExample from './app-launcher-sections-icons-example.hbs';
 import docs from '../docs/code.md';
 
 export const Docs = docs;
 
-export default (props) => {
+export default props => {
   const AppLauncherCollapsedExample = ApplauncherCollapsedExample();
   const AppLauncherExpandedExample = ApplauncherExpandedExample();
+  const AppLauncherSectionsIconsExample = ApplauncherSectionsIconsExample();
   const headingText = 'App launcher';
   const variablesRoot = 'pf-c-app-launcher';
 
@@ -22,6 +25,13 @@ export default (props) => {
       </Example>
       <Example heading="App launcher expanded" handlebars={AppLauncherExpandedExampleRaw} minHeight="20em">
         {AppLauncherExpandedExample}
+      </Example>
+      <Example
+        heading="App launcher w/ sections and icons"
+        handlebars={AppLauncherSectionsIconsExampleRaw}
+        minHeight="20em"
+      >
+        {AppLauncherSectionsIconsExample}
       </Example>
     </Documentation>
   );
