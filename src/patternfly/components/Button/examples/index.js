9880b7abecbf41e46d631212f1f770d113f9ab89
Christie Molloy <cmolloy@redhat.com>
2019-07-01 16:51:32 -0400

    feat(button): add background for focus

--- src/patternfly/components/Button/examples/index.js
+++ src/patternfly/components/Button/examples/index.js
@@ -6,11 +6,13 @@ import ButtonStatesTemplateRaw from '!raw!./button-states-example.hbs';
 import ButtonBlockTemplateRaw from '!raw!./button-block-example.hbs';
 import ButtonTypesTemplateRaw from '!raw!./button-types-example.hbs';
 import ButtonLinkExampleRaw from '!raw!./button-link-example.hbs';
+import ButtonBackgroundExampleRaw from '!raw!./button-background-example.hbs';
 import ButtonVariationTemplate from './button-variations-example.hbs';
 import ButtonStatesTemplate from './button-states-example.hbs';
 import ButtonBlockTemplate from './button-block-example.hbs';
 import ButtonTypesTemplate from './button-types-example.hbs';
 import ButtonLinkExample from './button-link-example.hbs';
+import ButtonBackgroundExample from './button-background-example.hbs';
 import docs from '../docs/code.md';
 
 export const Docs = docs;
@@ -19,6 +21,7 @@ export default props => {
   const buttonVariationTemplate = ButtonVariationTemplate();
   const buttonStatesTemplate = ButtonStatesTemplate();
   const buttonLinkExample = ButtonLinkExample();
+  const buttonBackgroundExample = ButtonBackgroundExample();
   const buttonBlockTemplate = ButtonBlockTemplate();
   const buttonTypesTemplate = ButtonTypesTemplate();
   const headingText = 'Button';
@@ -34,6 +37,13 @@ export default props => {
       </Example>
       <Example heading="Links as buttons" handlebars={ButtonLinkExampleRaw}>
         {buttonLinkExample}
+      </Example>
+      <Example
+        heading="Plain button with background in masthead"
+        handlebars={ButtonBackgroundExampleRaw}
+        className="is-dark-preview pf-t-dark"
+      >
+        {buttonBackgroundExample}
       </Example>
       <Example heading="Button (block level)" handlebars={ButtonBlockTemplateRaw}>
         {buttonBlockTemplate}
