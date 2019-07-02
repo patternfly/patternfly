d197c33ccd722a149d156f7f5467b84a569317bb
Christie Molloy <cmolloy@redhat.com>
2019-07-02 16:22:49 -0400

    feat(button): add variation for plain in the masthead

--- src/patternfly/components/NotificationBadge/examples/index.js
+++ src/patternfly/components/NotificationBadge/examples/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import notificationBadgeSimpleExampleRaw from '!raw!./notification-badge-simple-example.hbs';
+import notificationBadgeMastheadExampleRaw from '!raw!./notification-badge-masthead-example.hbs';
 import NotificationbadgeSimpleExample from './notification-badge-simple-example.hbs';
+import NotificationbadgeMastheadExample from './notification-badge-masthead-example.hbs';
 import docs from '../docs/code.md';
 
 export const Docs = docs;
 
 export default props => {
   const notificationBadgeSimpleExample = NotificationbadgeSimpleExample();
+  const notificationBadgeMastheadExample = NotificationbadgeMastheadExample();
   const headingText = 'Notification badge';
   const variablesRoot = 'pf-c-notification-badge';
 
@@ -16,6 +19,13 @@ export default props => {
     <Documentation data={props} docs={Docs} heading={headingText} variablesRoot={variablesRoot}>
       <Example heading="Notification badge" handlebars={notificationBadgeSimpleExampleRaw}>
         {notificationBadgeSimpleExample}
+      </Example>
+      <Example
+        heading="Notification badge masthead"
+        handlebars={notificationBadgeMastheadExampleRaw}
+        className="is-dark-preview pf-t-dark"
+      >
+        {notificationBadgeMastheadExample}
       </Example>
     </Documentation>
   );
