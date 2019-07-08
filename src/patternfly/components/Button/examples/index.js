3f5f1c6baf43e7a8fdbba93964b6c67cf7552b67
Christie Molloy <cmolloy@redhat.com>
2019-07-08 09:42:58 -0400

    fix(notificationbadge): move styles to the page and remove from button

--- src/patternfly/components/Button/examples/index.js
+++ src/patternfly/components/Button/examples/index.js
@@ -6,13 +6,11 @@ import ButtonStatesTemplateRaw from '!raw!./button-states-example.hbs';
 import ButtonBlockTemplateRaw from '!raw!./button-block-example.hbs';
 import ButtonTypesTemplateRaw from '!raw!./button-types-example.hbs';
 import ButtonLinkExampleRaw from '!raw!./button-link-example.hbs';
-import ButtonMastheadExampleRaw from '!raw!./button-masthead-example.hbs';
 import ButtonVariationTemplate from './button-variations-example.hbs';
 import ButtonStatesTemplate from './button-states-example.hbs';
 import ButtonBlockTemplate from './button-block-example.hbs';
 import ButtonTypesTemplate from './button-types-example.hbs';
 import ButtonLinkExample from './button-link-example.hbs';
-import ButtonMastheadExample from './button-masthead-example.hbs';
 import docs from '../docs/code.md';
 
 export const Docs = docs;
@@ -21,7 +19,6 @@ export default props => {
   const buttonVariationTemplate = ButtonVariationTemplate();
   const buttonStatesTemplate = ButtonStatesTemplate();
   const buttonLinkExample = ButtonLinkExample();
-  const buttonMastheadExample = ButtonMastheadExample();
   const buttonBlockTemplate = ButtonBlockTemplate();
   const buttonTypesTemplate = ButtonTypesTemplate();
   const headingText = 'Button';
@@ -37,13 +34,6 @@ export default props => {
       </Example>
       <Example heading="Links as buttons" handlebars={ButtonLinkExampleRaw}>
         {buttonLinkExample}
-      </Example>
-      <Example
-        heading="Plain button with background in masthead"
-        handlebars={ButtonMastheadExampleRaw}
-        className="is-dark-preview pf-t-dark"
-      >
-        {buttonMastheadExample}
       </Example>
       <Example heading="Button (block level)" handlebars={ButtonBlockTemplateRaw}>
         {buttonBlockTemplate}
--- src/patternfly/components/NotificationBadge/examples/index.js
+++ src/patternfly/components/NotificationBadge/examples/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import notificationBadgeSimpleExampleRaw from '!raw!./notification-badge-simple-example.hbs';
-import notificationBadgeMastheadExampleRaw from '!raw!./notification-badge-masthead-example.hbs';
 import NotificationbadgeSimpleExample from './notification-badge-simple-example.hbs';
-import NotificationbadgeMastheadExample from './notification-badge-masthead-example.hbs';
 import docs from '../docs/code.md';
 
 export const Docs = docs;
 
 export default props => {
   const notificationBadgeSimpleExample = NotificationbadgeSimpleExample();
-  const notificationBadgeMastheadExample = NotificationbadgeMastheadExample();
   const headingText = 'Notification badge';
   const variablesRoot = 'pf-c-notification-badge';
 
@@ -19,13 +16,6 @@ export default props => {
     <Documentation data={props} docs={Docs} heading={headingText} variablesRoot={variablesRoot}>
       <Example heading="Notification badge" handlebars={notificationBadgeSimpleExampleRaw}>
         {notificationBadgeSimpleExample}
-      </Example>
-      <Example
-        heading="Notification badge masthead"
-        handlebars={notificationBadgeMastheadExampleRaw}
-        className="is-dark-preview pf-t-dark"
-      >
-        {notificationBadgeMastheadExample}
       </Example>
     </Documentation>
   );
