afe1924bcb0a76763ffa31c311a5be05da1a7521
Michael Coker <mcoker@redhat.com>
2019-07-17 17:30:29 -0500

    fix(drawer): fix a11y, add zindex to main

--- src/patternfly/demos/Drawer/examples/index.js
+++ src/patternfly/demos/Drawer/examples/index.js
@@ -3,10 +3,8 @@ import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import drawerCollapsedExampleRaw from '!raw!./drawer-collapsed-example.hbs';
 import drawerExpandedExampleRaw from '!raw!./drawer-expanded-example.hbs';
-import drawerExpandedInlineExampleRaw from '!raw!./drawer-expanded-inline-example.hbs';
 import DrawerCollapsedExample from './drawer-collapsed-example.hbs';
 import DrawerExpandedExample from './drawer-expanded-example.hbs';
-import DrawerExpandedInlineExample from './drawer-expanded-inline-example.hbs';
 import docs from '../docs/code.md';
 
 export const Docs = docs;
@@ -14,7 +12,6 @@ export const Docs = docs;
 export default props => {
   const drawerCollapsedExample = DrawerCollapsedExample();
   const drawerExpandedExample = DrawerExpandedExample();
-  const drawerExpandedInlineExample = DrawerExpandedInlineExample();
   const headingText = 'Drawer';
   const variablesRoot = 'pf-d-drawer';
 
@@ -25,9 +22,6 @@ export default props => {
       </Example>
       <Example heading="Drawer expanded" handlebars={drawerExpandedExampleRaw} fullPageOnly="true">
         {drawerExpandedExample}
-      </Example>
-      <Example heading="Drawer expanded inline" handlebars={drawerExpandedInlineExampleRaw} fullPageOnly="true">
-        {drawerExpandedInlineExample}
       </Example>
     </Documentation>
   );
