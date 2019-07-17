0881d3bd0248a4002547268931cb852c53b181f9
Michael Coker <mcoker@redhat.com>
2019-07-17 16:18:36 -0500

    fix(drawer): pf-m-expanded, min-width on main, transition margin-right

--- src/patternfly/components/Drawer/examples/index.js
+++ src/patternfly/components/Drawer/examples/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import drawerClosedExampleRaw from '!raw!./drawer-closed-example.hbs';
-import drawerOpenExampleRaw from '!raw!./drawer-open-example.hbs';
-import drawerOpenInlineExampleRaw from '!raw!./drawer-open-inline-example.hbs';
+import drawerExpandedExampleRaw from '!raw!./drawer-expanded-example.hbs';
+import drawerExpandedInlineExampleRaw from '!raw!./drawer-expanded-inline-example.hbs';
 import DrawerClosedExample from './drawer-closed-example.hbs';
-import DrawerOpenExample from './drawer-open-example.hbs';
-import DrawerOpenInlineExample from './drawer-open-inline-example.hbs';
+import DrawerExpandedExample from './drawer-expanded-example.hbs';
+import DrawerExpandedInlineExample from './drawer-expanded-inline-example.hbs';
 import docs from '../docs/code.md';
 
 export const Docs = docs;
 
 export default props => {
   const drawerClosedExample = DrawerClosedExample();
-  const drawerOpenExample = DrawerOpenExample();
-  const drawerOpenInlineExample = DrawerOpenInlineExample();
+  const drawerExpandedExample = DrawerExpandedExample();
+  const drawerExpandedInlineExample = DrawerExpandedInlineExample();
   const headingText = 'Drawer';
   const variablesRoot = 'pf-c-drawer';
 
@@ -23,11 +23,11 @@ export default props => {
       <Example heading="Drawer closed" handlebars={drawerClosedExampleRaw}>
         {drawerClosedExample}
       </Example>
-      <Example heading="Drawer open" handlebars={drawerOpenExampleRaw}>
-        {drawerOpenExample}
+      <Example heading="Drawer expanded" handlebars={drawerExpandedExampleRaw}>
+        {drawerExpandedExample}
       </Example>
-      <Example heading="Drawer open inline" handlebars={drawerOpenInlineExampleRaw}>
-        {drawerOpenInlineExample}
+      <Example heading="Drawer expanded inline" handlebars={drawerExpandedInlineExampleRaw}>
+        {drawerExpandedInlineExample}
       </Example>
     </Documentation>
   );
--- src/patternfly/demos/Drawer/examples/index.js
+++ src/patternfly/demos/Drawer/examples/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import drawerCollapsedExampleRaw from '!raw!./drawer-collapsed-example.hbs';
-import drawerOpenExampleRaw from '!raw!./drawer-open-example.hbs';
-import drawerOpenInlineExampleRaw from '!raw!./drawer-open-inline-example.hbs';
+import drawerExpandedExampleRaw from '!raw!./drawer-expanded-example.hbs';
+import drawerExpandedInlineExampleRaw from '!raw!./drawer-expanded-inline-example.hbs';
 import DrawerCollapsedExample from './drawer-collapsed-example.hbs';
-import DrawerOpenExample from './drawer-open-example.hbs';
-import DrawerOpenInlineExample from './drawer-open-inline-example.hbs';
+import DrawerExpandedExample from './drawer-expanded-example.hbs';
+import DrawerExpandedInlineExample from './drawer-expanded-inline-example.hbs';
 import docs from '../docs/code.md';
 
 export const Docs = docs;
 
 export default props => {
   const drawerCollapsedExample = DrawerCollapsedExample();
-  const drawerOpenExample = DrawerOpenExample();
-  const drawerOpenInlineExample = DrawerOpenInlineExample();
+  const drawerExpandedExample = DrawerExpandedExample();
+  const drawerExpandedInlineExample = DrawerExpandedInlineExample();
   const headingText = 'Drawer';
   const variablesRoot = 'pf-d-drawer';
 
@@ -23,11 +23,11 @@ export default props => {
       <Example heading="Drawer collapsed" handlebars={drawerCollapsedExampleRaw} fullPageOnly="true">
         {drawerCollapsedExample}
       </Example>
-      <Example heading="Drawer open" handlebars={drawerOpenExampleRaw} fullPageOnly="true">
-        {drawerOpenExample}
+      <Example heading="Drawer expanded" handlebars={drawerExpandedExampleRaw} fullPageOnly="true">
+        {drawerExpandedExample}
       </Example>
-      <Example heading="Drawer open inline" handlebars={drawerOpenInlineExampleRaw} fullPageOnly="true">
-        {drawerOpenInlineExample}
+      <Example heading="Drawer expanded inline" handlebars={drawerExpandedInlineExampleRaw} fullPageOnly="true">
+        {drawerExpandedInlineExample}
       </Example>
     </Documentation>
   );
