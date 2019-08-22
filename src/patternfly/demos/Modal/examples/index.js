1c61d738806746aaea1493e998c0edefd9f02e44
Michael Coker <mcoker@redhat.com>
2019-08-22 17:39:46 -0500

    fix(modal): add left aligned footer buttons variation

--- src/patternfly/demos/Modal/examples/index.js
+++ src/patternfly/demos/Modal/examples/index.js
@@ -3,18 +3,21 @@ import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import ModalExampleRaw from '!raw!./modal-example.hbs';
 import ModalScrollExampleRaw from '!raw!./modal-scroll-example.hbs';
-import ModalLgExampleRaw from '!raw!./modal-lg-example.hbs';
+import ModalLgExampleRaw from '!raw!./modal-left-aligned-footer-example.hbs';
+import ModalLeftAlignedFooterExampleRaw from '!raw!./modal-lg-example.hbs';
 import docs from '../docs/code.md';
 import ModalExample from './modal-example.hbs';
 import ModalScrollExample from './modal-scroll-example.hbs';
 import ModalLgExample from './modal-lg-example.hbs';
+import ModalLeftAlignedFooterExample from './modal-left-aligned-footer-example.hbs';
 
 export const Docs = docs;
 
-export default (props) => {
+export default props => {
   const modalExample = ModalExample();
   const modalScrollExample = ModalScrollExample();
   const modalLgExample = ModalLgExample();
+  const modalLeftAlignedFooterExample = ModalLeftAlignedFooterExample();
   const headingText = 'Modal demo';
 
   return (
@@ -31,6 +34,13 @@ export default (props) => {
       </Example>
       <Example heading="Modal demo - large" fullPageOnly="true" handlebars={ModalLgExampleRaw}>
         {modalLgExample}
+      </Example>
+      <Example
+        heading="Modal demo - left aligned footer buttons"
+        fullPageOnly="true"
+        handlebars={ModalLeftAlignedFooterExampleRaw}
+      >
+        {modalLeftAlignedFooterExample}
       </Example>
     </Documentation>
   );
