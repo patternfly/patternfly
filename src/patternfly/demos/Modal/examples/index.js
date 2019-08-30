dbe25cc194f3b122103e8e7dee5a7ac583d5fa60
Michael Coker <35148959+mcoker@users.noreply.github.com>
2019-08-30 13:14:32 -0500

    fix(modal): add left aligned footer buttons variation (#2182)
    * fix(modal): add left aligned footer buttons variation
    
    * fix(modal): update demo button order
    
    * fix(modal): left align as default, add right align example

--- src/patternfly/demos/Modal/examples/index.js
+++ src/patternfly/demos/Modal/examples/index.js
@@ -3,18 +3,21 @@ import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
 import ModalExampleRaw from '!raw!./modal-example.hbs';
 import ModalScrollExampleRaw from '!raw!./modal-scroll-example.hbs';
-import ModalLgExampleRaw from '!raw!./modal-lg-example.hbs';
+import ModalLgExampleRaw from '!raw!./modal-right-aligned-footer-example.hbs';
+import ModalRightAlignedFooterExampleRaw from '!raw!./modal-lg-example.hbs';
 import docs from '../docs/code.md';
 import ModalExample from './modal-example.hbs';
 import ModalScrollExample from './modal-scroll-example.hbs';
 import ModalLgExample from './modal-lg-example.hbs';
+import ModalRightAlignedFooterExample from './modal-right-aligned-footer-example.hbs';
 
 export const Docs = docs;
 
-export default (props) => {
+export default props => {
   const modalExample = ModalExample();
   const modalScrollExample = ModalScrollExample();
   const modalLgExample = ModalLgExample();
+  const modalRightAlignedFooterExample = ModalRightAlignedFooterExample();
   const headingText = 'Modal demo';
 
   return (
@@ -31,6 +34,13 @@ export default (props) => {
       </Example>
       <Example heading="Modal demo - large" fullPageOnly="true" handlebars={ModalLgExampleRaw}>
         {modalLgExample}
+      </Example>
+      <Example
+        heading="Modal demo - legacy footer button alignment (right aligned)"
+        fullPageOnly="true"
+        handlebars={ModalRightAlignedFooterExampleRaw}
+      >
+        {modalRightAlignedFooterExample}
       </Example>
     </Documentation>
   );
