10dc5ac18a3db6245f8a1e4f546ca0489a012337
Matt Nolting <matthew.nolting@gmail.com>
2019-09-05 10:38:51 -0400

    feat(overflow-menu): add overflow menu (#2126)

---
+++ src/patternfly/components/OverflowMenu/docs/overflow-menu-simple-additional-options.md
@@ -0,0 +1,6 @@
+When additional options in the overflow menu exist, the overflow dropdown persists. When the viewport expands beyond the prescribed breakpoint, the overflow dropdown remains visible.
+
+### Overflow menu has additional options
+| Class | Applied to | Outcome |
+| -- | -- | -- |
+| `.pf-m-additional-options` | `.pf-c-overflow-menu__control` | Modifies overflow menu control visibility. |
---
+++ src/patternfly/components/OverflowMenu/examples/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Documentation from '@siteComponents/Documentation';
+import Example from '@siteComponents/Example';
+
+import overflowMenuSimpleExampleRaw from '!raw!./overflow-menu-simple-example.hbs';
+import overflowMenuGroupTypesExampleRaw from '!raw!./overflow-menu-group-types-example.hbs';
+import overflowMenuSimpleAdditionalOptionsExampleRaw from '!raw!./overflow-menu-simple-additional-options-example.hbs';
+import overflowMenuPersistentExampleRaw from '!raw!./overflow-menu-persistent-example.hbs';
+
+import OverflowMenuSimpleExample from './overflow-menu-simple-example.hbs';
+import OverflowMenuGroupTypesExample from './overflow-menu-group-types-example.hbs';
+import OverflowMenuSimpleAdditionalOptionsExample from './overflow-menu-simple-additional-options-example.hbs';
+import OverflowMenuPersistentExample from './overflow-menu-persistent-example.hbs';
+
+import overflowMenuSimpleExampleDoc from '../docs/overflow-menu-simple.md';
+import overflowMenuGroupTypesExampleDoc from '../docs/overflow-menu-group-types.md';
+import overflowMenuSimpleAdditionalOptionsExampleDoc from '../docs/overflow-menu-simple-additional-options.md';
+import overflowMenuPersistentExampleDoc from '../docs/overflow-menu-persistent.md';
+import overflowMenuIntroDoc from '../docs/overflow-menu-intro.md';
+
+import docs from '../docs/code.md';
+
+export const Docs = docs;
+
+export default props => {
+  const overflowMenuSimpleExample = OverflowMenuSimpleExample();
+  const overflowMenuGroupTypesExample = OverflowMenuGroupTypesExample();
+  const overflowMenuSimpleAdditionalOptionsExample = OverflowMenuSimpleAdditionalOptionsExample();
+  const overflowMenuPersistentExample = OverflowMenuPersistentExample();
+  const headingText = 'Overflow menu';
+  const variablesRoot = 'pf-c-overflow-menu';
+
+  return (
+    <Documentation data={props} docs={Docs} heading={headingText} variablesRoot={variablesRoot}>
+      <Example
+        intro={overflowMenuIntroDoc}
+        heading="Overflow menu simple (responsive)"
+        handlebars={overflowMenuSimpleExampleRaw}
+        docs={overflowMenuSimpleExampleDoc}
+        className="is-layout-page"
+      >
+        {overflowMenuSimpleExample}
+      </Example>
+      <Example
+        heading="Overflow menu group types"
+        handlebars={overflowMenuGroupTypesExampleRaw}
+        docs={overflowMenuGroupTypesExampleDoc}
+        className="is-layout-page"
+      >
+        {overflowMenuGroupTypesExample}
+      </Example>
+      <Example
+        heading="Overflow menu multiple groups - additional options"
+        handlebars={overflowMenuSimpleAdditionalOptionsExampleRaw}
+        docs={overflowMenuSimpleAdditionalOptionsExampleDoc}
+      >
+        {overflowMenuSimpleAdditionalOptionsExample}
+      </Example>
+      <Example
+        heading="Overflow menu persistent - additional options"
+        handlebars={overflowMenuPersistentExampleRaw}
+        docs={overflowMenuPersistentExampleDoc}
+        className="is-layout-page"
+      >
+        {overflowMenuPersistentExample}
+      </Example>
+    </Documentation>
+  );
+};
