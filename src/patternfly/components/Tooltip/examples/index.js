1f45f4cca369ea70834d957e4bd97f874640af34
Matt Nolting <mnolting@redhat.com>
2019-09-18 13:18:40 -0400

    feat(tooltip-alignment): add text align left

--- src/patternfly/components/Tooltip/examples/index.js
+++ src/patternfly/components/Tooltip/examples/index.js
@@ -6,21 +6,24 @@ import TooltipTopExampleRaw from '!raw!./tooltip-top-example.hbs';
 import TooltipRightExampleRaw from '!raw!./tooltip-right-example.hbs';
 import TooltipBottomExampleRaw from '!raw!./tooltip-bottom-example.hbs';
 import TooltipLeftExampleRaw from '!raw!./tooltip-left-example.hbs';
+import TooltipTextAlignLeftExampleRaw from '!raw!./tooltip-text-align-left-example.hbs';
 
 import TooltipTopExample from './tooltip-top-example.hbs';
 import TooltipRightExample from './tooltip-right-example.hbs';
 import TooltipBottomExample from './tooltip-bottom-example.hbs';
 import TooltipLeftExample from './tooltip-left-example.hbs';
+import TooltipTextAlignLeftExample from './tooltip-text-align-left-example.hbs';
 
 import docs from '../docs/code.md';
 
 export const Docs = docs;
 
-export default (props) => {
+export default props => {
   const tooltipTopExample = TooltipTopExample();
   const tooltipRightExample = TooltipRightExample();
   const tooltipBottomExample = TooltipBottomExample();
   const tooltipLeftExample = TooltipLeftExample();
+  const tooltipTextAlignLeftExample = TooltipTextAlignLeftExample();
   const headingText = 'Tooltip';
   const variablesRoot = 'pf-c-tooltip';
 
@@ -37,6 +40,9 @@ export default (props) => {
       </Example>
       <Example className="is-tooltip" heading="Tooltip left" handlebars={TooltipLeftExampleRaw}>
         {tooltipLeftExample}
+      </Example>
+      <Example className="is-tooltip" heading="Tooltip left align text" handlebars={TooltipTextAlignLeftExampleRaw}>
+        {tooltipTextAlignLeftExample}
       </Example>
     </Documentation>
   );
