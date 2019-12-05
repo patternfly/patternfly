1a7e26eff6a00d5b0dac1b918c454ca4865de126
redallen <zallen@redhat.com>
2019-12-05 11:16:05 -0500

    add gatsby-browser

---
+++ gatsby-browser.js
@@ -0,0 +1,19 @@
+import './dist/patternfly.css';
+// Utilities
+import './dist/patternfly-addons.css';
+// Experimental components
+import './dist/components/DataToolbar/data-toolbar.css';
+import './dist/components/Divider/divider.css';
+import './dist/components/Drawer/drawer.css';
+import './dist/components/OverflowMenu/overflow-menu.css';
+import './dist/components/Spinner/spinner.css';
+// React-specific CSS
+import '@patternfly/react-styles/src/css/components/Table/inline-edit.css';
+import '@patternfly/react-styles/src/css/components/Tooltip/tippy.css';
+import '@patternfly/react-styles/src/css/components/Tooltip/tippy-overrides.css';
+import '@patternfly/react-styles/src/css/components/Topology/topology-controlbar.css';
+import '@patternfly/react-styles/src/css/components/Topology/topology-side-bar.css';
+import '@patternfly/react-styles/src/css/components/Topology/topology-view.css';
+import '@patternfly/react-styles/src/css/layouts/Toolbar/toolbar.css';
+// Global theme CSS
+import 'gatsby-theme-patternfly-org/global.css';
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,3 +4,15 @@ const { mdxTypeDefs } = require('gatsby-theme-patternfly-org');
 exports.createSchemaCustomization = ({ actions }) => {
   actions.createTypes(mdxTypeDefs);
 }
+
+// Maybe we'll need this one day.
+// exports.onCreateWebpackConfig = ({ actions }) => {
+//   const distFolder = path.resolve(__dirname, 'dist');
+//   actions.setWebpackConfig({
+//     resolve: {
+//       alias: {
+//         '@patternfly/patternfly': distFolder
+//       }
+//     }
+//   })
+// }
