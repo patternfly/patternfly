feb7014ecbd0e2beb9b3f7274beaa1e18aa5a248
zallen <47335686+redallen@users.noreply.github.com>
2020-01-23 10:48:04 -0500

    build(lint): enabled linting dist css (#2586)

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const { mdxTypeDefs } = require('gatsby-theme-patternfly-org');
 // https://www.gatsbyjs.org/docs/schema-customization/
 exports.createSchemaCustomization = ({ actions }) => {
   actions.createTypes(mdxTypeDefs);
-}
+};
 
 // Maybe we'll need this one day.
 // exports.onCreateWebpackConfig = ({ actions }) => {
