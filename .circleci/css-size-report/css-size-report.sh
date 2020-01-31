3fc649edf98900fbb787849495d0506c945e0a0c
ddonahue007 <ddonahue@redhat.com>
2020-01-31 09:05:16 -0500

    chore(fix): css size chart
    added cli args to script for flex

--- .circleci/css-size-report/css-size-report.sh
+++ .circleci/css-size-report/css-size-report.sh
@@ -4,8 +4,30 @@
 #   - install previous version of patternfly to .circleci/css-size-report/nodemodules.
 #   - run nodeJS script that will compare current changed css file sizes to the previous version.
 #
-
 cd $(dirname $0)
+
+# process cli args
+usage() { echo "Usage: $0 [-c <run|clean>] [-p <package_name>]" 1>&2; exit 1; }
+
+while getopts ":c:p:" o; do
+    case "${o}" in
+        c)
+            CMD=${OPTARG}
+            ((s == run || s == clean)) || usage
+            ;;
+        p)
+            PACKAGE=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${CMD}" ] || [ -z "${PACKAGE}" ]; then
+    usage
+fi
 
 # log levels
 INFO="INFO"
@@ -23,10 +45,9 @@ logErr() {
 }
 
 setup() {
-  if [ ! -d "./node_modules/@patternfly/patternfly" ]; then
-    logMsg "setup previous version of patternfly for css comparisons"
-
-    if npm i -D @patternfly/patternfly; then
+  if ! npm ls ${PACKAGE} > /dev/null 2>&1; then
+    logMsg "installing previous version of patternfly for css comparisons"
+    if npm i -D ${PACKAGE}; then
       logMsg "setup ran successfully"
     else
       logErr "setup failed"
@@ -48,6 +69,18 @@ run() {
   fi
 }
 
+clean() {
+  logMsg "cleaning .circleci/css-size-report/node_modules"
+  npm uninstall -D ${PACKAGE}
+  rm -rf node_modules
+}
+
 ## execute
-setup
-run
+if [ "${CMD}" == "run" ]; then
+  setup
+  run
+elif [ "${CMD}" == "clean" ]; then
+  clean
+fi
+
+
