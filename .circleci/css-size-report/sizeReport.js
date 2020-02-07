1e2418283e1d7c9836bac865e9cdaa587633bee4
ddonahue007 <ddonahue@redhat.com>
2020-02-07 12:30:32 -0500

    chore(fix): css size chart
    Moved logic form bash script to JS script.
    fixed percentage logic. we now display colors associated with neg\pos values.

--- .circleci/css-size-report/sizeReport.js
+++ .circleci/css-size-report/sizeReport.js
@@ -1,43 +1,38 @@
-const myArgs = process.argv.slice(2);
-const package = myArgs[0];
+#!/usr/bin/env node
 
 const fs = require('fs');
 const glob = require('glob');
-const Octokit = require('@octokit/rest');
+const { Octokit }  = require('@octokit/rest');
+const path = require('path');
+const CWD =  process.cwd();
 
-const octokit = new Octokit({ auth: process.env.GH_PR_TOKEN });
+let exitCode = 0;
 
-const owner = process.env.CIRCLE_PROJECT_USERNAME; // patternfly
-const repo = process.env.CIRCLE_PROJECT_REPONAME;
-const prnum = process.env.CIRCLE_PR_NUMBER;
+// process command line args
+const argv = require('yargs')
+  .usage(`Usage:  ${path.basename(__filename)} <command> [options]`)
+  .command('run', 'Run the report', () => {}, (argv) => {
+    run('@patternfly/patternfly');
+  })
+  .command('clean', 'Clean up anything the report tool as built out',  () => {}, (argv) => {
+    clean();
+  })
+  .showHelpOnFail(true)
+  .strict()
+  .demandCommand(1, '')
+  .help('h')
+  .alias('h', 'help').argv
 
-const exitCode = 0;
-const results = {};
-
-// the number of files that have changed
-let totalFiles = 0;
-// the color of the result size value.
-let sizeCol = 'green';
-
-let html = '<!DOCTYPE html>';
-
-html += '<head>';
-html += '<style>'
-html += `th { font-weight:bold; background-color: #e6e6e6; }`
-html += `table, th, td { border: 1px solid black; border-collapse: collapse; }`
-html += '</style>';
-html += '</head>';
-
-html += '<html>';
-html += '<body>';
-
-html += '<table style="width:100%">';
-html += '<tr>';
-html += `<th>Name</th>`;
-html += `<th>Current(kb)</th>`;
-html += `<th>Previous(kb)</th>`;
-html += `<th>Diff %</th>`;
-html += '</tr>';
+function setUp(package) {
+  try{
+    if (! fs.existsSync('./node_modules')) {
+      const execSync = require('child_process').execSync;
+      execSync('npm i -D ' + package + "> /dev/null 2>&1", { cwd: CWD, encoding: 'utf-8' });
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
 
 // build a Map object(<key> = file, <value> = size in kb), sorting highest to lowest.
 function buildValueMap(searchPattern, topLvlPattern) {
@@ -51,7 +46,6 @@ function buildValueMap(searchPattern, topLvlPattern) {
       if (normalized.startsWith(topLvlPattern)) {
         normalized = file.split('/').pop();
       }
-
       m.set(normalized, fs.statSync(file).size);
     });
 
@@ -61,6 +55,32 @@ function buildValueMap(searchPattern, topLvlPattern) {
 
 // compare value maps.
 function compareMaps(currValues, prevValues) {
+  const results = {};
+  // the number of files that have changed
+  let totalFiles = 0;
+  // the color of the result size value.
+  let sizeCol = 'green';
+
+  let html = '<!DOCTYPE html>';
+
+  html += '<head>';
+  html += '<style>'
+  html += `th { font-weight:bold; background-color: #e6e6e6; }`
+  html += `table, th, td { border: 1px solid black; border-collapse: collapse; }`
+  html += '</style>';
+  html += '</head>';
+
+  html += '<html>';
+  html += '<body>';
+
+  html += '<table style="width:100%">';
+  html += '<tr>';
+  html += `<th>Name</th>`;
+  html += `<th>Current(kb)</th>`;
+  html += `<th>Previous(kb)</th>`;
+  html += `<th>Diff %</th>`;
+  html += '</tr>';
+
   currValues.forEach((size, file) => {
     if (size >= 10000) {
       sizeCol = '#E74C3C';
@@ -74,7 +94,7 @@ function compareMaps(currValues, prevValues) {
     let diff;
     if ( typeof prevValues.get(file) !== 'undefined' ) {
       psize = prevValues.get(file)
-      diff = size !== 0 ? Math.abs(((size - psize) / size) * 100).toPrecision(2) : 0;
+      diff = size !== 0 ? Math.round((size - psize) / size * 100) : 0;
     } else {
       psize = "-";
       diff = "-";
@@ -109,53 +129,76 @@ function compareMaps(currValues, prevValues) {
   html += '</html>';
 
   console.log(html);
+
+  const octokit = new Octokit({ auth: process.env.GH_PR_TOKEN });
+  const owner = process.env.CIRCLE_PROJECT_USERNAME; // patternfly
+  const repo = process.env.CIRCLE_PROJECT_REPONAME;
+  const prnum = process.env.CIRCLE_PR_NUMBER;
+
+  if (prnum) {
+    octokit.issues
+      .listComments({
+        owner,
+        repo,
+        issue_number: prnum
+      })
+      .then(res => res.data)
+      .then(comments => {
+        let commentBody = '';
+        const existingComment = comments.find(comment => comment.user.login === 'patternfly-build');
+        if (existingComment) {
+          commentBody += existingComment.body;
+        }
+
+        commentBody += '\n';
+        commentBody += html;
+
+        if (existingComment) {
+          octokit.issues
+            .updateComment({
+              owner,
+              repo,
+              comment_id: existingComment.id,
+              body: commentBody
+            })
+            .then(() => console.log('Updated comment!'));
+        } else {
+          octokit.issues
+            .createComment({
+              owner,
+              repo,
+              issue_number: prnum,
+              body: commentBody
+            })
+            .then(() => console.log('Created comment!'));
+        }
+      });
+  }
 }
 
-// build previous values
-const prevMap = buildValueMap('node_modules/' + package + '/**/*.css', package);
-const currMap = buildValueMap('../../dist/**/**/*.css', '../dist');
+function run(package) {
+  setUp(package);
+  const prevMap = buildValueMap(__dirname + '/node_modules/' + package + '/**/*.css', package);
+  const currMap = buildValueMap(__dirname + '/../../dist/**/**/*.css', 'patternfly-next/dist');
+  compareMaps(currMap, prevMap);
+}
 
-// compare Maps
-compareMaps(currMap, prevMap);
+function clean() {
+  const deleteFolderRecursive = (dir_path) => {
+    if (fs.existsSync(dir_path)) {
+      fs.readdirSync(dir_path).forEach((file, index) => {
+        const curPath = path.join(dir_path, file);
+        if (fs.lstatSync(curPath).isDirectory()) { // recurse
+          deleteFolderRecursive(curPath);
+        } else { // delete file
+          fs.unlinkSync(curPath);
+        }
+      });
+      fs.rmdirSync(dir_path);
+    }
+  };
 
-if (prnum) {
-  octokit.issues
-    .listComments({
-      owner,
-      repo,
-      issue_number: prnum
-    })
-    .then(res => res.data)
-    .then(comments => {
-      let commentBody = '';
-      const existingComment = comments.find(comment => comment.user.login === 'patternfly-build');
-      if (existingComment) {
-        commentBody += existingComment.body;
-      }
-
-      commentBody += '\n';
-      commentBody += html;
-
-      if (existingComment) {
-        octokit.issues
-          .updateComment({
-            owner,
-            repo,
-            comment_id: existingComment.id,
-            body: commentBody
-          })
-          .then(() => console.log('Updated comment!'));
-      } else {
-        octokit.issues
-          .createComment({
-            owner,
-            repo,
-            issue_number: prnum,
-            body: commentBody
-          })
-          .then(() => console.log('Created comment!'));
-      }
-    });
+  deleteFolderRecursive('./node_modules');
 }
 
 process.exit(exitCode);
