0ad2c6f8145ee6a1e2b12a883686b39951c24936
zallen <47335686+redallen@users.noreply.github.com>
2020-06-19 13:47:05 -0400

    chore(core): upgrade patternfly-a11y (#3178)

---
+++ patternfly-a11y.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  aggregate: true,
+  prefix: 'http://localhost:9000',
+  ignoreRules: 'color-contrast,page-has-heading-one,scrollable-region-focusable',
+  ignoreIncomplete: true
+};
