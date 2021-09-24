83c88e0269b6ac892a1b0263c85d27f3ae5d8c6d
evwilkin <evan.wilkinson@gmail.com>
2021-09-24 10:00:49 -0400

    fix(icons): copy unicode from PF3 icons, make new icon unicodes persist

--- scripts/gulp/icons.js
+++ scripts/gulp/icons.js
@@ -2,8 +2,13 @@ const { src, dest } = require('gulp');
 const iconfont = require('gulp-iconfont');
 const iconfontCss = require('gulp-iconfont-css');
 const generateIcons = require('../../src/icons/generateIcons.js');
+const existingIconCodes = require('../../src/icons/existingIconCodes.json');
+const fs = require('fs');
+const path = require('path');
 
 const pficonFontName = 'pficon';
+const nextCodePoint = Math.max(...existingIconCodes.codePoints) + 1;
+const nextUnicodeHex = '0x' + nextCodePoint.toString(16).toUpperCase();
 
 function pfIcons() {
   return generateIcons();
@@ -17,7 +22,11 @@ function pfIconFont() {
         path: 'scss',
         targetPath: 'pficon.scss',
         fontPath: './',
-        cssClass: 'pf-icon'
+        cssClass: 'pf-icon',
+        // Assign next available unicode (for new icons)
+        firstGlyph: nextUnicodeHex,
+        // Reference saved unicodes (for existing icons)
+        fixedCodepoints: existingIconCodes
       })
     )
     .pipe(
@@ -27,6 +36,23 @@ function pfIconFont() {
         timestamp: Math.round(Date.now() / 1000)
       })
     )
+    // gulp-iconfont emits a 'glyphs' event
+    // we use to update existingIconCodes file
+    .on('glyphs', function(glyphs, options) {
+      const newIconsObj = glyphs.reduce((iconsObj, glyph) => {
+        const { name, unicode } = glyph;
+        const codePoint = unicode[0].codePointAt(0);
+        iconsObj[name] = codePoint.toString(16).toUpperCase();
+        iconsObj.codePoints.push(codePoint);
+        return iconsObj;
+      }, {codePoints: []});
+
+      fs.writeFile(
+        path.join(process.cwd(), '/src/icons/existingIconCodes.json'),
+        JSON.stringify(newIconsObj),
+        err => err && console.log(err)
+      );
+    })
     .pipe(dest('./src/patternfly/assets/pficon/'));
 }
 
