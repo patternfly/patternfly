9c0085788739e8f3857e935c209af7fa439e89d1
Jeff Puzzo <jeffrey.puzzo@gmail.com>
2022-01-18 10:04:42 -0500

    chore: remove modifiers page

--- scripts/extractModifiers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#grep -r -i -o "pf-m-[a-zA-Z]*" --include \*.scss ./src/patternfly
-grep -i -o -h "pf-m-[a-zA-Z0-9-]*\b" `find ./src/patternfly -name \*.scss` \
-  | awk -F' ' '{printf "- "$1"\n"}' \
-  | sort -u > ./src/site/pages/modifiers.md
