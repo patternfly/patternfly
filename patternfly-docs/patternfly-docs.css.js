f8922aa31a8ac35422043801953ca5c6e974043f
Michael Coker <mcoker@redhat.comc>
2022-07-06 19:32:22 -0500

    chore(workspace): remove react css imports

--- patternfly-docs/patternfly-docs.css.js
+++ patternfly-docs/patternfly-docs.css.js
@@ -1,10 +1,3 @@
-// React-specific CSS
-import '@patternfly/react-styles/src/css/components/Table/inline-edit.css';
-import '@patternfly/react-styles/src/css/components/Topology/topology-controlbar.css';
-import '@patternfly/react-styles/src/css/components/Topology/topology-side-bar.css';
-import '@patternfly/react-styles/src/css/components/Topology/topology-view.css';
-import '@patternfly/react-styles/src/css/layouts/Toolbar/toolbar.css';
-
 // Patternfly
 import '../dist/patternfly.css';
 
