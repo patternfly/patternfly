290bade654edf210d4156772a5c4f15e213df18a
nicolethoen <nthoen@redhat.com>
2022-12-20 13:20:17 -0500

    feat(release): release v5 branch to alpha channel
    BREAKING CHANGE: 5.0.0-alpha

--- release.config.js
+++ release.config.js
@@ -1,19 +1,18 @@
 module.exports = {
-  branches: ['v5', { name: 'v5', channel: 'alpha' }],
+  branches: [
+    'main',
+    { name: 'main', channel: 'prerelease' },
+    { name: 'v5', channel: 'alpha', prerelease: 'alpha' }
+  ],
+  analyzeCommits: {
+    preset: 'angular'
+  },
   plugins: [
-    ['@semantic-release/commit-analyzer', {
-      preset: 'angular',
-      releaseRules: [
-        { tag: 'doc', release: 'patch' },
-        { tag: 'chore', release: 'patch' },
-        { tag: 'feat', release: 'patch' },
-        { tag: 'fix', release: 'patch' },
-        { tag: 'bug', release: 'patch' }
-      ]
-    }],
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     ['@semantic-release/npm', { pkgRoot: 'dist' }]
   ],
-  tagFormat: 'alpha-v${version}'
+  tagFormat: 'alpha-v${version}',
+  dryRun: 'true'
 };
