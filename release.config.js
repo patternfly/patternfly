d7c8f4bc5dbbba9da9d94d727e93134b77c5c353
nicolethoen <nthoen@redhat.com>
2023-06-02 10:36:00 -0400

    fix(release): dryrun true

--- release.config.js
+++ release.config.js
@@ -12,5 +12,6 @@ module.exports = {
     '@semantic-release/github',
     ['@semantic-release/npm', { pkgRoot: 'dist' }]
   ],
-  tagFormat: 'prerelease-v${version}'
+  tagFormat: 'prerelease-v${version}',
+  dryRun: true
 };
