98abe75e22c2cabeb5be4510d0217a008a5e206c
Nicole Thoen <nthoen@redhat.com>
2023-06-02 10:59:59 -0400

    chore(release): turn off dry run on v4

--- release.config.js
+++ release.config.js
@@ -12,6 +12,5 @@ module.exports = {
     '@semantic-release/github',
     ['@semantic-release/npm', { pkgRoot: 'dist' }]
   ],
-  tagFormat: 'prerelease-v${version}',
-  dryRun: true
+  tagFormat: 'prerelease-v${version}'
 };
