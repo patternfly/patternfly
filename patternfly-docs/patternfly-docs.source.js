937bb0898999a49735f7a4238606892196ec77dd
Nicole Thoen <nthoen@redhat.com>
2023-06-08 08:34:56 -0400

    chore(soureces): remove configs for release notes and upgrade guide

--- patternfly-docs/patternfly-docs.source.js
+++ patternfly-docs/patternfly-docs.source.js
@@ -22,11 +22,5 @@ module.exports = sourceMD => {
   sourceMD(utilitiesMdFiles, 'html');
   sourceMD(deprecatedComponentsMdFiles, 'html-deprecated');
 
-  sourceMD(path.join(coreDocsPath, 'demos/**/*.md'), 'html-demos');
-
-  // Release notes
-  sourceMD(require.resolve('../RELEASE-NOTES.md'), 'html');
-
-  // Upgrade guides
-  sourceMD(require.resolve('../UPGRADE-GUIDE.md'), 'html');
+  sourceMD(path.join(coreDocsPath, 'demos/**/*.md'), 'html-demos'
 };
