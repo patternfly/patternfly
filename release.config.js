56e1e0514d4f1777d44a9723ede51857344bcbf4
Nicole Thoen <nthoen@redhat.com>
2023-08-03 16:33:35 -0400

    feat(breaker): bump this version to 6.0 (#5800)
    BREAKING CHANGE: 6.0.0-alpha.0

--- release.config.js
+++ release.config.js
@@ -2,7 +2,8 @@ module.exports = {
   branches: [
     'do-not-delete',
     { name: 'v4', channel: 'prerelease-v4', range: '4.x' },
-    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' }
+    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
+    { name: 'v6', channel: 'alpha', prerelease: 'alpha' }
   ],
   analyzeCommits: {
     preset: 'angular'
@@ -13,5 +14,6 @@ module.exports = {
     '@semantic-release/github',
     ['@semantic-release/npm', { pkgRoot: 'dist' }]
   ],
-  tagFormat: 'prerelease-v${version}'
+  tagFormat: 'alpha-v${version}',
+  dryRun: true
 };
