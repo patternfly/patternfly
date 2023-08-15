56b4f3fe8772e77130cc94d6d46ff3dd38d63cea
Michael Coker <35148959+mcoker@users.noreply.github.com>
2023-08-15 16:01:57 -0500

    feat(global): added RTL support (#5778)

---
+++ backstop.js
@@ -0,0 +1,52 @@
+const config = require('./backstop.config');
+const scenarios = [];
+const viewports = [];
+
+config.relativeUrls.map(relativeUrl => {
+  const url = (relativeUrl.url || relativeUrl);
+  const fullUrl = `${config.baseUrl}${url}`;
+  return scenarios.push({
+    label: url,
+    url: fullUrl,
+    delay: (relativeUrl.delay || 100), // a small timeout allows wiggle room for the page to fully render. increase as needed if you're getting rendering related false positives.
+    readySelector: '.ws-theme-switch-full-page',
+    removeSelectors: ['.ws-theme-switch-full-page'],
+    misMatchThreshold: 0.1
+  })
+});
+
+Object.keys(config.viewports).map(viewport => (
+  viewports.push({
+    name: viewport,
+    width: config.viewports[viewport].width,
+    height: config.viewports[viewport].height
+  })
+));
+
+module.exports = {
+  id: 'pf-core',
+  viewports,
+  scenarios,
+  onReadyScript: 'onReady.js',
+  report: ['browser'],
+  engine: 'puppeteer',
+  engineOptions: {
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox'
+    ]
+  },
+  paths: {
+    bitmaps_reference: 'backstop_data/bitmaps_reference',
+    bitmaps_test: 'backstop_data/bitmaps_test',
+    engine_scripts: 'backstop_data/engine_scripts',
+    html_report: 'backstop_data/html_report',
+    ci_report: 'backstop_data/ci_report'
+  },
+  asyncCaptureLimit: 5,
+  asyncCompareLimit: 50,
+  debug: false,
+  debugWindow: false,
+  archiveReport: false,
+  scenarioLogsInReports: false
+};
