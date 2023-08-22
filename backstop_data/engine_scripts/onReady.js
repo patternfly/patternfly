175f82a5268a324c5698c02840b0e7e8280cc48e
mcoker <mcoker@redhat.com>
2023-08-22 11:32:07 -0500

    chore: rtl fixes, screenshots

--- backstop_data/engine_scripts/onReady.js
+++ backstop_data/engine_scripts/onReady.js
@@ -1,4 +1,3 @@
 module.exports = async (page) => {
-  // disable animations/transitions so they don't interfere with screenshot tool
-  await page.addStyleTag({content: "*, *::before, *::after {animation-delay:-1ms !important;animation-duration:1ms !important;animation-iteration-count:1 !important;transition-duration:0s !important;transition-delay:0s !important;}"});
+  // scripts go here
 };
