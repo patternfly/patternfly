ef1ac3e3631e4a77cfeb9a6e5c5862e921a5e7de
Nicole Thoen <nthoen@redhat.com>
2023-08-30 15:56:45 -0400

    fix: add quotes around branch name in release config

--- release.config.js
+++ release.config.js
@@ -3,7 +3,7 @@ module.exports = {
     'do-not-delete',
     { name: 'v4', channel: 'prerelease-v4', range: '4.x' },
     { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
-    { name: 5.0.x, channel: 'prerelease-bugfix', range: '5.0.x' }
+    { name: '5.0.x', channel: 'prerelease-bugfix', range: '5.0.x' }
   ],
   analyzeCommits: {
     preset: 'angular'
