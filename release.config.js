73685ad0b050492e08309c3ba840ba93b1c7161f
Nicole Thoen <nthoen@redhat.com>
2023-10-06 14:55:18 -0400

    fix(release): dry run patch releases (#5978)
    * fix(release): dry run patch releases
    
    * fix(release): update pr-preview to fix gh action

--- release.config.js
+++ release.config.js
@@ -2,16 +2,27 @@ module.exports = {
   branches: [
     'do-not-delete',
     { name: 'v4', channel: 'prerelease-v4', range: '4.x' },
-    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' }
+    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
+    { name: '5.1.x', channel: 'prerelease-bugfix', range: '5.1.x' }
   ],
   analyzeCommits: {
     preset: 'angular'
   },
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'feat', release: 'patch' },
+          { type: 'fix', release: 'patch' }
+        ]
+      }
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     ['@semantic-release/npm', { pkgRoot: 'dist' }]
   ],
-  tagFormat: 'prerelease-v${version}'
+  tagFormat: 'patch-v${version}',
+  dryRun: true
 };
