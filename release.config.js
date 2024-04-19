1dae39beb27a6eefdd158c6997ef10054c2b6f51
Donald Labaj <dlabaj@redhat.com>
2024-04-19 16:41:35 -0400

    chore(setup-release): Updated semantic release for 5.3.x

--- release.config.js
+++ release.config.js
@@ -2,17 +2,27 @@ module.exports = {
   branches: [
     'do-not-delete',
     { name: 'v4', channel: 'prerelease-v4', range: '4.x' },
-    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' }
+    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
+    { name: '5.3.x', channel: 'prerelease-bugfix', range: '5.3.x'}
   ],
   analyzeCommits: {
     preset: 'angular'
   },
   plugins: [
-    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'feat', release: 'patch' },
+          { type: 'fix', release: 'patch' }
+        ]
+      }
+    ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     ['@semantic-release/npm', { pkgRoot: 'dist' }]
   ],
-  tagFormat: 'prerelease-v${version}',
-  dryRun: false
+  tagFormat: 'patch-v${version}',
+  dryRun: true
 };
