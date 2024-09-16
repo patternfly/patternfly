50afcae4bc324ed597b402a923973486c97ac76a
Donald Labaj <donald.labaj@gmail.com>
2024-09-16 15:13:16 -0400

    fix: Fixed the release so it release prerelease.

--- .history/release.config_20240916151226.js
+++ .history/release.config_20240916151226.js
@@ -3,7 +3,7 @@ module.exports = {
     'do-not-delete',
     { name: 'v4', channel: 'prerelease-v4', range: '4.x' },
     { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
-    { name: 'v5', channel: 'prerelease-v5', range: '5.x' }
+    { name: 'v5', channel: 'prerelease-v5', range: '5.4.x' }
   ],
   analyzeCommits: {
     preset: 'angular'
