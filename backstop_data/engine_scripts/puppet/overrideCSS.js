6524f561754bdf43b55ab056af7e0d78d6acbf08
Michael Coker <35148959+mcoker@users.noreply.github.com>
2025-06-11 15:41:58 -0500

    chore(backstop): use pf-m-no-motion, table screenshots (#7570)

--- backstop_data/engine_scripts/puppet/overrideCSS.js
+++ backstop_data/engine_scripts/puppet/overrideCSS.js
@@ -1,12 +1,6 @@
-// disable animations/transitions
-const CSS_OVERRIDE = '*, *::before, *::after {animation-delay:-1ms !important;animation-duration:1ms !important;animation-iteration-count:1 !important;transition-duration:0s !important;transition-delay:0s !important;}';
-
 module.exports = async (page, scenario) => {
-  await page.evaluate(`window._styleData = '${CSS_OVERRIDE}'`);
   await page.evaluate(() => {
-    const style = document.createElement('style');
-    const styleNode = document.createTextNode(window._styleData);
-    style.appendChild(styleNode);
-    document.head.appendChild(style);
+    // disable animations/transitions
+    document.querySelector('html').classList.add('pf-m-no-motion');
   });
 };
