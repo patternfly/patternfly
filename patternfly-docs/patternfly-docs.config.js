175534c3600f8ec1b188b1ebf49adf75ba080bc9
Eric Olkowski <git.eric@thatblindgeye.dev>
2025-08-27 15:18:56 -0400

    chore(Config): updated hasDarkThemeSwitcher var

--- patternfly-docs/patternfly-docs.config.js
+++ patternfly-docs/patternfly-docs.config.js
@@ -3,7 +3,7 @@ module.exports = {
   hasGdprBanner: false,
   hasFooter: false,
   hasVersionSwitcher: false,
-  hasDarkThemeSwitcher: true,
+  hasThemeSwitcher: true,
   hasRTLSwitcher: true,
   sideNavItems: [
     { section: 'get-started' },
