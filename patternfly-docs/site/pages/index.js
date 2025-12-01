9123d7e65bdb41a3533362947f44362fe5e9a8aa
mcoker <mcoker@redhat.com>
2025-12-01 16:25:16 -0600

    chore: fix broken links

--- patternfly-docs/site/pages/index.js
+++ patternfly-docs/site/pages/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { PageSection } from '@patternfly/react-core';
-import quickRefPdf from './reference-docs/PF-quick-ref.pdf';
 
 const HomePage = () => (
   <PageSection variant="light">
-    Just getting started? Take a look at the <a href="/contribution">Contribution guide</a> and{' '}
-    <a href="/guidelines">Coding guidelines.</a> Always keep our <a href="/accessibility-guide">Accessibility guide</a>{' '}
-    in mind. Download a <a href={quickRefPdf}>quick reference sheet.</a>
+    Just getting started? Take a look at the <a href="/contribution">contribution guide</a> and{' '}
+    <a href="/guidelines">coding guidelines</a>, and always keep our <a href="https://www.patternfly.org/accessibility/about-accessibility">accessibility fundamentals</a>{' '}
+    in mind.
   </PageSection>
 );
 
