e18bc4156fb18b24d1f2d7f50c03ba2b85810e4b
mcoker <mcoker@redhat.com>
2025-12-02 12:52:14 -0600

    chore: guidelines link text update

--- patternfly-docs/site/pages/index.js
+++ patternfly-docs/site/pages/index.js
@@ -4,7 +4,7 @@ import { PageSection } from '@patternfly/react-core';
 const HomePage = () => (
   <PageSection variant="light">
     Just getting started? Take a look at the <a href="/contribution">contribution guide</a> and{' '}
-    <a href="/guidelines">coding guidelines</a>, and always keep our <a href="https://www.patternfly.org/accessibility/about-accessibility">accessibility fundamentals</a>{' '}
+    <a href="/guidelines">development guidelines</a>, and always keep our <a href="https://www.patternfly.org/accessibility/about-accessibility">accessibility fundamentals</a>{' '}
     in mind.
   </PageSection>
 );
