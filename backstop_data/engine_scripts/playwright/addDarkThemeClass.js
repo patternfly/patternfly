94490c98f79533062c1dae26c0aa6f680c573c4e
Michael Coker <mcoker@redhat.com>
2026-04-03 21:43:14 -0500

    chore: remove unused scripts

--- backstop_data/engine_scripts/playwright/addDarkThemeClass.js
+++
@@ -1,9 +0,0 @@
-// add dark theme class to html tag
-const darkThemeClass = 'pf-v6-theme-dark';
-
-module.exports = async (page, scenario) => {
-  await page.evaluate(() => {
-    const root = document.querySelector('html');
-    root.classList.add('pf-v6-theme-dark');
-  });
-};
