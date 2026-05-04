020cbc6bab0dfe80da2fb1891eeaf8c3fee9744b
Michael Coker <mcoker@redhat.com>
2026-05-04 15:55:03 -0500

    chore: back out stuff theme switcher needs

--- backstop_data/engine_scripts/playwright/onBefore.js
+++ backstop_data/engine_scripts/playwright/onBefore.js
@@ -28,7 +28,8 @@ module.exports = async (page, scenario, vp, isReference, Engine, config) => {
 
     // Set theme variant preference
     if (options.felt) {
-      localStorage.setItem('theme-variant-preference', 'theme-felt');
+      // localStorage.setItem('theme-variant-preference', 'theme-felt');
+      localStorage.setItem('theme-variant-preference', 'theme-redhat'); // TODO - remove once theme switcher updates to use felt storage item
     } else {
       localStorage.setItem('theme-variant-preference', 'theme-default');
     }
