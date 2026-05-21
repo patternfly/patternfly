811b7c913cf79d76a0e4fcb6c34967937d7b9877
Donald Labaj <donald.labaj@gmail.com>
2026-05-21 09:24:22 -0400

    fix: updated for patch releases

--- release.config.js
+++ release.config.js
@@ -2,25 +2,26 @@ module.exports = {
   branches: [
     'do-not-delete',
     { name: 'v5', channel: 'prerelease-v5', range: '5.x' },
-    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' }
+    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
+    { name: '6.5.x', channel: 'prerelease-bugfix', range: '6.5.x' }
   ],
   analyzeCommits: {
     preset: 'angular'
   },
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer',
       {
         preset: 'angular',
-        parserOpts: {
-          noteKeywords: ['BREAKING-CHANGE']
-        }
+        releaseRules: [
+          { type: 'feat', release: 'patch' },
+          { type: 'fix', release: 'patch' }
+        ]
       }
     ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     ['@semantic-release/npm', { pkgRoot: 'dist' }]
   ],
-  tagFormat: 'prerelease-v${version}',
-  dryRun: false
+  tagFormat: 'patch-v${version}',
+  dryRun: true
 };
