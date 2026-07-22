33202db4818dae597d29c510216aaecae4d1b345
Donald Labaj <donald.labaj@gmail.com>
2026-07-22 10:05:11 -0400

    chore: updated with commit chages

--- release.config.js
+++ release.config.js
@@ -2,7 +2,8 @@ module.exports = {
   branches: [
     'do-not-delete',
     { name: 'v5', channel: 'prerelease-v5', range: '5.x' },
-    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' }
+    { name: 'main', channel: 'prerelease', prerelease: 'prerelease' },
+    { name: '6.6.x', channel: 'prerelease-bugfix', range: '6.6.x' }
   ],
   analyzeCommits: {
     preset: 'angular'
@@ -12,15 +13,16 @@ module.exports = {
       '@semantic-release/commit-analyzer',
       {
         preset: 'angular',
-        parserOpts: {
-          noteKeywords: ['BREAKING-CHANGE']
-        }
+        releaseRules: [
+          { type: 'feat', release: 'patch' },
+          { type: 'fix', release: 'patch' }
+        ]
       }
     ],
     '@semantic-release/release-notes-generator',
     '@semantic-release/github',
     ['@semantic-release/npm', { pkgRoot: 'dist' }]
   ],
-  tagFormat: 'prerelease-v${version}',
+  tagFormat: 'patch-v${version}',
   dryRun: false
 };
